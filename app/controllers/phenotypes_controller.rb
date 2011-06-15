28f31d600b41de5bbcb4a673a3a4ce8808c3782a
Philipp <PhilippBay@gmail.com>
2011-06-15 16:31:55 +1000

    Made the phenotype-controller nicer to read

--- app/controllers/phenotypes_controller.rb
+++ app/controllers/phenotypes_controller.rb
@@ -1,8 +1,9 @@
 class PhenotypesController < ApplicationController
 
 	def show
-		@variations = Phenotype.find(params[:id]).variations
-		@user = User.find_by_id(Phenotype.find(params[:id]).user_id)
+		@phenotype = Phenotype.find(params[:id])
+		@variations = @phenotype.variations
+		@user = User.find_by_id(@phenotype.user_id)
 	
 		@title = "Phenotypes"
 		respond_to do |format|
