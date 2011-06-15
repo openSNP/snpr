886ff4e0a0bf349d88f14a03317e7cd2e522ce8c
Philipp <PhilippBay@gmail.com>
2011-06-15 15:26:37 +1000

    Added Phenotypes-controller with simple change function which returns the array/hash of variations

--- app/controllers/phenotypes_controller.rb
+++ app/controllers/phenotypes_controller.rb
@@ -1,2 +1,13 @@
 class PhenotypesController < ApplicationController
+
+	def change
+		# let's hope this works
+		@variations = Phenotype.find_by_user_id(User.find(params[:id]))
+
+		respond_to do |format|
+			format.html
+			format.xml
+		end
+	end
+
 end
