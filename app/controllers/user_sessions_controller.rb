efe469fc2262ccffa748e4c739cee38b3c312384
Philipp <PhilippBay@gmail.com>
2011-06-17 16:59:42 +1000

    On login, user now arrives at show, not index

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
 		@user_session = UserSession.new(params[:user_session])
 		if @user_session.save
 			flash[:notice] = "Login successful!"
-			redirect_to root_url
+			redirect_back_or_default account_url
 		else
 			render :action => :new
 		end
