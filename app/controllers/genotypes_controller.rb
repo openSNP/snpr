6ac1e59c2b2eb43cff13ef9bdc556d34e61bfd40
Bastian Greshake <bgreshake@googlemail.com>
2011-07-05 12:06:38 +0200

    modified upload-controller

--- app/controllers/genotypes_controller.rb
+++ app/controllers/genotypes_controller.rb
@@ -75,13 +75,14 @@ class GenotypesController < ApplicationController
 		new_snps.each do |single_snp|
 			if single_snp[0] != "#"
 				snp_array = single_snp.split("\t")
-				@snp = Snp.new()
-				@snp.genotype_id = @genotype.id
-				@snp.name = snp_array[0]
-				@snp.chromosome = snp_array[1]
-				@snp.position = snp_array[2]
-				@snp.save
-			end
-   	    end
+				  if snp_array.length == (4)
+				    @snp = Snp.new()
+				    @snp.name = snp_array[0]
+				    @snp.chromosome = snp_array[1]
+				    @snp.position = snp_array[2]
+				    @snp.save
+			    end
+		    end
+   	  end
   end	
 end
