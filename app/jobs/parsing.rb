93723a510f2a30c6d827e41b92dba143eb49c318
Bastian Greshake <bgreshake@googlemail.com>
2011-07-16 14:18:56 +0200

    finished 23andme-parser

--- app/jobs/parsing.rb
+++ app/jobs/parsing.rb
@@ -5,23 +5,29 @@ class Parsing
 
 	def self.perform(genotyp)
 	  @genotype = Genotype.find_by_id(genotyp["genotype"]["id"].to_i)
-	  puts @genotype.fs_filename
-	  puts "hello"
-	  puts ::Rails.root.to_s
+	  
 		genotype_file = File.open(::Rails.root.to_s+"/public/data/"+ @genotype.fs_filename, "r")
 		new_snps = genotype_file.readlines
 		new_snps.each do |single_snp|
-			if single_snp[0][0] != "#"
+			if single_snp[0][0].chr != "#"
 				snp_array = single_snp.split("\t")
 				puts snp_array[0]
 				  if snp_array.length == (4)
-				    @snp = Snp.new()
-				    @snp.name = snp_array[0]
-				    @snp.chromosome = snp_array[1]
-				    @snp.position = snp_array[2]
-				    @snp.save
-			    	  end
-		    	end
+				    if Snp.find_by_name(snp_array[0]) == nil
+				      @snp = Snp.new()
+				      @snp.name = snp_array[0]
+				      @snp.chromosome = snp_array[1]
+				      @snp.position = snp_array[2]
+				      @snp.save
+			      end
+			      @user_snp = UserSnp.new()
+			      @user_snp.genotype_id = @genotype.id
+			      @user_snp.user_id = @genotype.user_id
+			      @user_snp.snp_id = @snp.id
+			      @user_snp.local_genotype = snp_array[3]
+			      @user_snp.save
+			    end
+		  end
 		end
 	end
 end	
