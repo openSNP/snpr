e03b9f70dfcaab724cba24f3399c4b1a9ed90c63
Philipp <PhilippBay@gmail.com>
2011-08-16 18:56:17 +1000

    Removed deprecated filtering in application-controller, moved to config/application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :all
   helper_method :current_user_session, :current_user
-  filter_parameter_logging :password, :password_confirmation
+  
+  #filter_parameter_logging :password, :password_confirmation
 
   private
 
