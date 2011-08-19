51b51a6aecf417935ba8e27ba3d5433756df3873
Bastian Greshake <bgreshake@googlemail.com>
2011-08-19 18:42:06 +0200

    added a small code-snippet to demonstrate how plos-api can be used

---
+++ api_plos.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'rexml/document'
+
+api_key = "INSERT_YOUR_CODE_HERE"
+
+url = "http://api.plos.org/search?q="+ARGV[0]+"&api_key="+api_key
+xml_data = Net::HTTP.get_response(URI.parse(url)).body
+
+doc = REXML::Document.new(xml_data)
+
+all_elements = doc.elements.to_a
+
+all_elements[0][2].each do |singleton|
+  first_author = singleton[2][0].to_s.gsub!(/<\/?str>/,"")
+  print "first author:" +first_author+"\n"
+  doi = singleton[4].to_s.gsub!(/<\/?str( name='id')?>/,"")
+  print "doi: "+doi+"\n"
+  pub_date = singleton[6].to_s.gsub!(/<\/?date( name='publication_date')?>/,"")
+  print "pub. date: "+pub_date+"\n"
+  title = singleton[7].to_s.gsub!(/<\/?str( name='title')?>/,"")
+  print "title: "+title+"\n"
+end
