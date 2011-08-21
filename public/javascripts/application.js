d6ff311664ca4310fcd4acae71ce4676c2892777
Bastian Greshake <bgreshake@googlemail.com>
2011-08-21 18:27:18 +0200

    added sortable tables to papers and snp-overview

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() 
+    { 
+        $("#PaperMendeley").tablesorter({sortList: [[2,1]]}); 
+		$("#PaperPlos").tablesorter({sortList: [[2,1]]});
+		$("#snp_overview").tablesorter();
+    } 
+);
