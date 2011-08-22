2fe64b66f679a36086fcd73401c4d92eeed7f740
Bastian Greshake <bgreshake@googlemail.com>
2011-08-22 18:22:22 +0200

    small proof of concept for sortable, will_paginate-table

--- app/controllers/snps_controller.rb
+++ app/controllers/snps_controller.rb
@@ -1,4 +1,5 @@
 class SnpsController < ApplicationController
+  helper_method :sort_column, :sort_direction
 	def index
 		@snps = Snp.paginate(:page => params[:page])
 
@@ -24,10 +25,24 @@ class SnpsController < ApplicationController
     Resque.enqueue(Plos,@snp)
     Resque.enqueue(Mendeley,@snp)
 	  Resque.enqueue(Snpedia,@snp)
+	  
+	  @plos_papers = @snp.plos_paper.order(sort_column + " " + sort_direction)
+	  @page_plos_papers = @plos_papers.paginate(:page => params[:page],:per_page => 4)
+	  
 		respond_to do |format|
 			format.html
 			format.xml
 		end
 	end
+	
+	private
+	
+	def sort_column
+    PlosPaper.column_names.include?(params[:sort]) ? params[:sort] : "pub_date"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	#def mark_required(object, attribute)
 	#	"*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator
 	#end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column+"#plos", :direction => direction}, {:class => css_class}
+  end	
+
+	
 end
