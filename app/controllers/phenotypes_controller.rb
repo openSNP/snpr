15732515af2021d7179799ea4cb4a8a54e0ffdb7
Philipp <PhilippBay@gmail.com>
2011-08-30 18:02:00 +1000

    Now possible to add phenotypes. Only problem: Right now, not possible to enter/save variation at the same time

--- app/controllers/phenotypes_controller.rb
+++ app/controllers/phenotypes_controller.rb
@@ -1,5 +1,30 @@
 class PhenotypesController < ApplicationController
     before_filter :require_user
+	
+	def new
+		@phenotype = Phenotype.new
+		@title = "Create a new phenotype"
+
+		respond_to do |format|
+			format.html
+			format.xml
+		end
+	end
+
+	def create
+		@phenotype = Phenotype.new(params[:phenotype])
+		# check if phenotype exists
+		if Phenotype.find_by_characteristic(@phenotype.characteristic) == nil
+			@phenotype.save
+		end
+        
+		@user_phenotype = UserPhenotype.new(:user_id => current_user.id, :phenotype_id => @phenotype.id, :variation => "")
+		if @user_phenotype.save
+			redirect_to current_user
+		else
+			redirect_to :action => "new", :notice => "Something went wrong in creating the phenotype"
+		end
+	end
 
 	def show
 		@phenotypes = Phenotype.where(:user_id => current_user.id).all
