06ec3111b2e8dcfa1353b3614ab2d556f451b227
Bastian Greshake <bgreshake@googlemail.com>
2011-09-07 11:46:02 +0200

    fuck, deleting does not work

--- app/jobs/deletegenotype.rb
+++ app/jobs/deletegenotype.rb
@@ -4,9 +4,13 @@ class Deletegenotype
    @queue = :deletegenotype
 
    def self.perform(genotyp)
-      @genotype = Genotype.find_by_id(genotyp["genotype"]["id"].to_i)
+     print genotyp["genotype"]["id"].to_i
+     print Genotype.find_by_id(genotyp["genotype"]["id"].to_i)
+     @genotype = Genotype.find_by_id(genotyp["genotype"]["id"].to_i) 
+     print "got genotype-file\n"
+     print @genotype
 	  # find all relevant user_snps
-	  @user_snps = UserSnp.find_by_user_id(@genotype.user_id)
+	  @user_snps = UserSnp.find_all_by_user_id(@genotype.user_id)
 	  # now parse through all user_snps, delete the relevant SNP if the user_snp is the only one, then delete the user_snp
 	  @user_snps.each do |us|
 		  @snp = Snp.find_by_id(us.snp_id)
