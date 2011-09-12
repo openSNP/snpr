132bea0b2fb8c0dcd7b25cf0131195f80d52b913
Bastian Greshake <bgreshake@googlemail.com>
2011-09-12 23:23:58 +0200

    set default values

--- db/migrate/20110615045458_create_phenotypes.rb
+++ db/migrate/20110615045458_create_phenotypes.rb
@@ -3,7 +3,7 @@ class CreatePhenotypes < ActiveRecord::Migration
 	  create_table :phenotypes do |p|
 		  p.string :characteristic  # e.g. haircolor
 		  p.string :known_phenotypes
-		  p.integer :number_of_users
+		  p.integer :number_of_users, :default => 0
 		  p.timestamps
 	  end
   end
