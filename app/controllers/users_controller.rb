1a05299b8537a706253b121b16d6f10c4c135bc0
Bastian Greshake <bgreshake@googlemail.com>
2011-09-30 15:15:12 +0200

    require owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :require_owner, only: [ :update, :destroy, :edit ]
+  before_filter :require_owner, only: [ :update, :destroy, :edit, :changepassword ]
   helper_method :sort_column, :sort_direction
   before_filter :require_no_user, :only => [:new, :create]
 
