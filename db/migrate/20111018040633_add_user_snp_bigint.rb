81de52e8ccb787df0a43cb9eca15880384f85978
Philipp <PhilippBay@gmail.com>
2011-10-18 14:43:54 +1000

    Changed UserSnp to type bigint, seems to work - not putting on server yet

---
+++ db/migrate/20111018040633_add_user_snp_bigint.rb
@@ -0,0 +1,9 @@
+class AddUserSnpBigint < ActiveRecord::Migration
+  def self.up
+		change_column :user_snps, :id, :integer, :limit => 8
+  end
+
+  def self.down
+		change_column :user_snps, :id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111007145000) do
+ActiveRecord::Schema.define(:version => 20111018040633) do
 
   create_table "achievements", :force => true do |t|
     t.text     "award"
