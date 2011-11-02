c78447afc4b59badd4a14dc0b6dc836179da8413
Philipp <PhilippBay@gmail.com>
2011-11-02 13:52:35 +1000

    db-migration for snpedia-links now sets all revision-ids to 0 so that first run of the parser actually works

--- db/migrate/20111102033039_add_revision_to_snpedia_paper.rb
+++ db/migrate/20111102033039_add_revision_to_snpedia_paper.rb
@@ -1,6 +1,9 @@
 class AddRevisionToSnpediaPaper < ActiveRecord::Migration
   def self.up
-		add_column :snpedia_papers, :revision, :int
+		add_column :snpedia_papers, :revision, :int, :default => 0
+		SnpediaPaper.all.each do |paper|
+			paper.update_attributes(:revision => 0)
+		end
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111029180506) do
+ActiveRecord::Schema.define(:version => 20111028212506) do
 
   create_table "achievements", :force => true do |t|
     t.text     "award"
