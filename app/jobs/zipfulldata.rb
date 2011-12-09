6df72d5bdad02bb7af86d136cd41ca107a5853b9
Bastian Greshake <bgreshake@googlemail.com>
2011-12-09 21:14:18 +0100

    wrote a small resque-task which creates a zip with all genotyping-files and a csv of all phenotypes (only users which have uploaded genotypings are included)

--- app/jobs/zipfulldata.rb
+++ app/jobs/zipfulldata.rb
@@ -4,51 +4,71 @@ class Zipfulldata
   @queue = :zipfulldata
 
   def self.perform(target_address)
-    @user_phenotypes = UserPhenotype.find_all_by_phenotype_id_and_variation(phenotype_id,variation)
     @genotyping_files = []
     @users = []
-    
+
     Genotype.find_each do |g|
       @genotyping_files << g
       @users << User.find(g.user_id)
     end
     
-    @csv_head = "user_id;chrom_sex;date_of_birth"
-    @phenotype_id_array = []
+    # only try to create csv & zip-file if there is data at all. 
     
-    Phenotype.find_each do |p|
-      @phenotype_id_array << p.id
-      @csv_head = @csv_head + p.characteristic.gsub(";",",")
-    end
-    
-    #ruby-1.9.2-p290 :011 > User.find_each do |u|
-    #ruby-1.9.2-p290 :012 >     phenotype_id_array.each do |parray|
-    #ruby-1.9.2-p290 :013 >       puts UserPhenotype.find_by_user_id_and_phenotype_id(u.id,parray)
-    #ruby-1.9.2-p290 :014?>     end
-    #ruby-1.9.2-p290 :015?>   puts "next user"
-    #ruby-1.9.2-p290 :016?>   end
-    
-    @user_phenotypes.each do |up|
-      @user = User.find_by_id(up.user_id)
-      print @user
-      if @user.genotypes[0] != nil
-        @genotyping_files << @user.genotypes[0]
-      end
-    end
-
     if @genotyping_files != []
       @time = Time.now
-      if File.exists?(::Rails.root.to_s+"/public/data/zip/"+phenotype_id.to_s+"."+@time.to_s.gsub(" ","_")+".zip") == false
-        Zip::ZipFile.open(::Rails.root.to_s+"/public/data/zip/"+phenotype_id.to_s+"."+@time.to_s.gsub(" ","_")+".zip", Zip::ZipFile::CREATE) do |zipfile| 
+      
+      # only create a new file if in the current minute none has been created yet
+      
+      if File.exists?(::Rails.root.to_s+"/public/data/zip/datadump."+@time.to_s.gsub(" ","_")+".zip") == false
+        
+        #create csv-head and writes to csv-filehandle
+        
+        @csv_head = "user_id;chrom_sex;date_of_birth"
+        @csv_handle = File.new(::Rails.root.to_s+"/tmp/dump"+@time.to_s+".csv","w")
+        @phenotype_id_array = []
+    
+        Phenotype.find_each do |p|
+          @phenotype_id_array << p.id
+          @csv_head = @csv_head + ";" + p.characteristic.gsub(";",",")
+        end
+        @csv_handle.puts(@csv_head)
+    
+        # create lines in csv-file for each user who has uploaded his data
+        
+        @users.each do |u|
+          @user_line = u.id.to_s + ";" + u.yearofbirth + ";" + u.sex
+          @phenotype_id_array.each do |pid|
+            if UserPhenotype.find_by_user_id_and_phenotype_id(u.id,pid) != nil
+              @user_line = @user_line + ";" + UserPhenotype.find_by_user_id_and_phenotype_id(u.id,pid).variation.gsub(";",",")
+            else 
+              @user_line = @user_line + ";" + "-"
+            end          
+          end
+          @csv_handle.puts(@user_line)
+        end
+        
+        @csv_handle.close
+        puts "created csv-file"
+    
+        # zip up the everything (csv + all genotypings) 
+        
+        Zip::ZipFile.open(::Rails.root.to_s+"/public/data/zip/datadump."+@time.to_s.gsub(" ","_")+".zip", Zip::ZipFile::CREATE) do |zipfile|
+          zipfile.add("phenotypes_"+@time.to_s+".csv",::Rails.root.to_s+"/tmp/dump"+@time.to_s+".csv") 
           @genotyping_files.each do |gen_file|
             zipfile.add("user"+gen_file.user_id.to_s+"_file"+gen_file.id.to_s+"_yearofbirth"+gen_file.user.yearofbirth+"_sex"+gen_file.user.sex+"."+gen_file.filetype+".txt", ::Rails.root.to_s+"/public/data/"+ gen_file.fs_filename)
           end
         end
+        
+        File.delete(::Rails.root.to_s+"/tmp/dump"+@time.to_s+".csv")
+        puts "created zip-file"
       end
-      system("chmod 777 "+::Rails.root.to_s+"/public/data/zip/"+phenotype_id.to_s+"."+@time.to_s.gsub(" ","_")+".zip")
-      UserMailer.genotyping_results(target_address,"/data/zip/"+phenotype_id.to_s+"."+@time.to_s.gsub(" ","_")+".zip",Phenotype.find_by_id(phenotype_id).characteristic,variation).deliver
+      
+      # make sure the file-permissions of the resulting zip-file are okay and sent mail
+      system("chmod 777 "+::Rails.root.to_s+"/public/data/zip/datadump."+@time.to_s.gsub(" ","_")+".zip")
+      UserMailer.genotyping_results(target_address,"/data/zip/datadump."+@time.to_s.gsub(" ","_")+".zip","JUST A TEST", "JUST A TEST").deliver
+      puts "sent mail"
     else
-      UserMailer.no_genotyping_results(target_address,Phenotype.find_by_id(phenotype_id).characteristic,variation).deliver
+      UserMailer.no_genotyping_results(target_address,"JUST AN EMPTY TEST","JUST AN EMPTY TEST").deliver
     end
   end
 end
