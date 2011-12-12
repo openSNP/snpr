c0da3ecb93121c2fe27f678351460d9badf5eb6f
Philipp <PhilippBay@gmail.com>
2011-12-12 19:08:23 +1000

    Fixed genotype-deleting with several genotypes

--- app/controllers/genotypes_controller.rb
+++ app/controllers/genotypes_controller.rb
@@ -99,7 +99,7 @@ class GenotypesController < ApplicationController
 
   def destroy
     @user = current_user
-    @genotype = Genotype.find_by_user_id(@user.id)
+    @genotype = Genotype.find_by_id(params[:id])
     File.delete(::Rails.root.to_s+"/public/data/"+ @genotype.fs_filename)
     Resque.enqueue(Deletegenotype, @genotype)
     if @genotype.delete
