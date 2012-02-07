dcc55c4a27a271a9743e24a46b2d70f09e602833
Bastian Greshake <bgreshake@googlemail.com>
2012-02-07 14:47:44 +0100

    made /features possible with only segment but no coordinates on it

--- app/controllers/das_controller.rb
+++ app/controllers/das_controller.rb
@@ -5,20 +5,42 @@ class DasController < ApplicationController
         if params[:segment]
             @positions = params[:segment].split(":")
             @id = @positions[0] # the chromosome
-            @start_and_end = @positions[1].split(",")
-            @start = @start_and_end[0]
-            @end = @start_and_end[1]
-            # Get only those SNPs which have chromosome = id
-            # and where position is between start and end
-            #
-            # Needs some postgresql-magic to force types
             
-            @snps = @user.snps.where('CAST(position as integer) <= ? AND CAST(position as integer) >= ? AND CAST(chromosome as text) = ?', @end, @start, @id)
-            @user_snps = []
-            # ugly solution
-            @snps.each do |s|
-                # there is only one user_snp for each snp
+            @known_chromosomes =  ("1".."22").to_a << "X" << "MT" << "Y"
+            if @known_chromosomes.include? @id
+              @unkown_chromosome = false
+            else 
+              @unkown_chromosome = true
+            end
+            
+            if @positions[1] != nil
+              @start_and_end = @positions[1].split(",")
+              @start = @start_and_end[0]
+              @end = @start_and_end[1]
+              @has_start = true
+              # Get only those SNPs which have chromosome = id
+              # and where position is between start and end
+              #
+              # Needs some postgresql-magic to force types
+              
+              @snps = @user.snps.where('CAST(position as integer) <= ? AND CAST(position as integer) >= ? AND CAST(chromosome as text) = ?', @end, @start, @id)
+              @user_snps = []
+              # ugly solution
+              @snps.each do |s|
+                  # there is only one user_snp for each snp
+                  @user_snps << UserSnp.find_by_user_id_and_snp_name(@user.id, s.name)
+              end
+                
+            else
+              @snps = @user.snps.where('CAST(chromosome as text) = ?', @id)
+              @user_snps = []
+              # ugly solution
+              @snps.each do |s|
+              # there is only one user_snp for each snp
                 @user_snps << UserSnp.find_by_user_id_and_snp_name(@user.id, s.name)
+              end
+                
+              @has_start = false
             end
         else
             # no chromosome or start/end, so get nothing
