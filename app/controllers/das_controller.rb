c13703eaa21200e42f3b59da424b153c8b520175
Philipp Bayer <PhilippBay@gmail.com>
2012-02-07 18:59:51 +1000

    More work on POST using positions

--- app/controllers/das_controller.rb
+++ app/controllers/das_controller.rb
@@ -4,6 +4,17 @@ class DasController < ApplicationController
         @user = User.find_by_id(params[:id])
         @user_snps = @user.user_snps
         render :template => 'das/show.xml.erb', :layout => false
+        if params[:segment]
+            @positions = params[:segment].split(":")
+            @id = @positions[0]
+            @start_and_end = @positions[1].split(",")
+            @start = @start_and_end[0]
+            @end = @start_and_end[1]
+        else
+            @id = 1
+            @start = 2
+            @end  = 3
+        end
     end
 end
 
