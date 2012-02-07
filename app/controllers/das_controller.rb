c6ef6c77fc530d135a1fb1dfff3f5f3d3c7cea09
Philipp Bayer <PhilippBay@gmail.com>
2012-02-07 19:49:58 +1000

    More work on /features. id and start/end is properly shown, but still all snps from the user are fetched

--- app/controllers/das_controller.rb
+++ app/controllers/das_controller.rb
@@ -2,19 +2,20 @@ class DasController < ApplicationController
 
     def show
         @user = User.find_by_id(params[:id])
-        @user_snps = @user.user_snps
-        render :template => 'das/show.xml.erb', :layout => false
         if params[:segment]
+            @user_snps = @user.user_snps.find(:all)
             @positions = params[:segment].split(":")
             @id = @positions[0]
             @start_and_end = @positions[1].split(",")
             @start = @start_and_end[0]
             @end = @start_and_end[1]
         else
-            @id = 1
-            @start = 2
-            @end  = 3
+            @user_snps = @user.user_snps.find(:all)
+            @id = ""
+            @start = ""
+            @end  = ""
         end
+        render :template => 'das/show.xml.erb', :layout => false
     end
 end
 
