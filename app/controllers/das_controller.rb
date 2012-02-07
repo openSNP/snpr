ba099085cacc325c83f3d941eb323c85d537aed2
Philipp Bayer <PhilippBay@gmail.com>
2012-02-07 22:11:15 +1000

    Searching for SNPs using specific positions and ?segment=1:1,80533061 works

--- app/controllers/das_controller.rb
+++ app/controllers/das_controller.rb
@@ -3,14 +3,24 @@ class DasController < ApplicationController
     def show
         @user = User.find_by_id(params[:id])
         if params[:segment]
-            @user_snps = @user.user_snps.find(:all)
             @positions = params[:segment].split(":")
-            @id = @positions[0]
+            @id = @positions[0] # the chromosome
             @start_and_end = @positions[1].split(",")
             @start = @start_and_end[0]
             @end = @start_and_end[1]
+            # Get only those SNPs which have chromosome = id
+            # and where position is between start and end
+            #
+            # Needs some postgresql-magic to force types
+            @snps = @user.snps.where('CAST(position as integer) <= ? AND CAST(position as integer) >= ? AND CAST(chromosome as integer) = ?', @end, @start, @id)
+            @user_snps = []
+            # ugly solution
+            @snps.each do |s|
+                @user_snps << UserSnp.find_by_user_id_and_snp_name(@user.id, s.name)
+            end
         else
-            @user_snps = @user.user_snps.find(:all)
+            # no chromosome or start/end, so get all SNPs
+            @user_snps = @user.user_snps
             @id = ""
             @start = ""
             @end  = ""
