748d13903a959e67dbb6aa7ecc3a5648f2213ec5
Helge Rausch <helge@rausch.io>
2012-03-30 18:46:23 +0200

    starting solr before tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "authlogic/test_case"
-SunspotTest.solr_startup_timeout = 60
+SunspotTest.solr_startup_timeout = 30
 require 'sunspot_test/test_unit'
 
 class ActiveSupport::TestCase
