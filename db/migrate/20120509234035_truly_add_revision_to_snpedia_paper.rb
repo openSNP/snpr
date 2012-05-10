f94cf378ef50ef91985c4bc7e207ae664194fdb6
Philipp Bayer <PhilippBay@gmail.com>
2012-05-10 14:05:39 +1000

    Made revision-introduction more robust

---
+++ db/migrate/20120509234035_truly_add_revision_to_snpedia_paper.rb
@@ -0,0 +1,18 @@
+class TrulyAddRevisionToSnpediaPaper < ActiveRecord::Migration
+  def self.up
+      if column_exists? :snpedia_papers, :revision
+        remove_column :snpedia_papers, :revision
+      end
+
+      change_table :snpedia_papers do |s|
+          s.integer :revision, :default => 0
+      end
+      SnpediaPaper.all.each do |sp|
+          sp.update_attribute :revision, 0
+      end
+  end
+
+  def self.down
+        remove_column :snpedia_papers, :revision
+  end
+end
