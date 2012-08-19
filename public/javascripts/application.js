2d3241a94a962cffbf3aeb92bb221d504130e170
Philipp Bayer <PhilippBay@gmail.com>
2012-08-19 14:08:32 +1000

    Fixed New User modal disappearing properly. Cleaned up application-JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,26 +4,29 @@
 
 $(document).ready(function() 
     { 
-		$('#update_user').click(function() {
-			$('#update_user').val("Updating...").addClass("disabled");
-			setTimeout(function(){ $('#update_user').val("Update Information").removeClass("disabled");},1000);
-		});
+    $('#update_user').click(function() {
+        $('#update_user').val("Updating...").addClass("disabled");
+        setTimeout(function(){ $('#update_user').val("Update Information").removeClass("disabled");},1000);
+    });
 
     $("#PaperMendeley").tablesorter({sortList: [[2,1]]}); 
     $("#PaperPlos").tablesorter({sortList: [[2,1]]});
-    } 
-);
+    $('#remove_help_one').click(function() {
+        $('#help_one').hide('slow');
+    });$
+    $('#remove_help_two').click(function() {
+        $('#help_two').hide('slow');
+    });
+    $('#remove_help_three').click(function() {
+        $('#help_three').hide('slow');
+    });
 
-$(document).ready(function(){ 
     $('#tab-container').easytabs();
-});$ 
-
-$(document).ready(function(){
-$("body").bind("click", function (e) {
-$('.dropdown-toggle, .menu').parent("li").removeClass("open");
-});
-$(".dropdown-toggle, .menu").click(function (e) {
-var $li = $(this).parent("li").toggleClass('open');
-return false;
-});
+    $("body").bind("click", function (e) {
+    $('.dropdown-toggle, .menu').parent("li").removeClass("open");
+    });
+    $(".dropdown-toggle, .menu").click(function (e) {
+    var $li = $(this).parent("li").toggleClass('open');
+    return false;
+    });
 });
