c60e250ba521e3c191fa2268e08885a051bfa042
Philipp Bayer <PhilippBay@gmail.com>
2012-08-23 20:56:44 +1000

    Changed piechart-colours

--- test/factories.rb
+++ test/factories.rb
@@ -1,49 +1,37 @@
-FactoryGirl.define do
-    factory :user do |u|
+FactoryGirl.define :user do |u|
   u.name "Dogbert"
   u.sequence(:email) { |i| "fubert#{i}@example.org" }
   u.password "jeheim"
   u.password_confirmation "jeheim"
   u.sex 'yes please'
   u.yearofbirth '1970'
-    end
 end
 
-FactoryGirl.define do
-    factory :genotype do |g|
+FactoryGirl.define :genotype do |g|
     g.originalfilename "foo.txt"
     g.uploadtime { Time.now }
     g.association :user
-    end
 end
 
-FactoryGirl.define do
-  factory :snp do |s|
+FactoryGirl.define :snp do |s|
     s.sequence(:name) { |i| "name #{i}" }
     s.sequence(:position) { |i| i }
     s.sequence(:chromosome) { |i| i }
     s.genotype_frequency("AA" => 1)
     s.allele_frequency("A" => 0, "T" => 0, "G" => 0, "C" => 0)
     s.ranking 0
-  end
 end
 
-FactoryGirl.define do
-    factory :achievement do |a|
+FactoryGirl.define :achievement do |a|
   a.award "Foooooooo"
-    end
 end
 
-FactoryGirl.define do 
-    factory :phenotype do |p|
+FactoryGirl.define :phenotype do |p|
   p.characteristic "Penis length"
-    end
 end
 
-FactoryGirl.define do
-    factory :user_phenotype do |up|
+FactoryGirl.define :user_phenotype do |up|
   up.association :user
   up.association :phenotype
   up.variation "pink"
-    end
 end
