d53e07510adf1b40d66384689ff4bf70ea3757bc
drsnuggles <philippbay@gmail.com>
2012-09-06 14:15:55 +1000

    Introduced proper logging to preparser. Doesn't work (log  is undefined method?)

--- app/jobs/preparsing.rb
+++ app/jobs/preparsing.rb
@@ -4,59 +4,63 @@ class Preparsing
   @queue = :preparse
 
   def self.perform(genotype_id)
+    Rails.logger.level = 0
+    Rails.logger = Logger.new("#{Rails.root}/log/preparsing_#{Rails.env}.log")
     genotype_id = genotype_id["genotype"]["id"].to_i if genotype_id.is_a?(Hash)
     @genotype = Genotype.find(genotype_id)
     filename = "#{Rails.root}/public/data/#{@genotype.fs_filename}"
     
+    log "Starting preparse"
     begin
       Zip::ZipFile.foreach(filename) do |entry|
         # if decodeme-file try to find the csv-file that includes all the data
+        log "Checking for proper-filename"
         if @genotype.filetype == "decodeme"
           if entry.to_s.include?(".csv") == true
-            puts "decodeme: found csv-file"
+            log "decodeme: found csv-file"
             Zip::ZipFile.open(filename) {
               |zipfile|
               zipfile.extract(entry,"#{Rails.root}/tmp/#{@genotype.fs_filename}.csv")
               zipfile.close()
-              puts "extracted file"
+              log "extracted file"
             }
             system("mv #{Rails.root}/tmp/#{@genotype.fs_filename}.csv #{Rails.root}/public/data/#{@genotype.fs_filename}")
-            puts "copied file"
+            log "copied file"
           end
         
         elsif @genotype.filetype == "23andme"
-          puts "23andme"
+          log "23andme"
           if entry.to_s.include?("genome") == true
-            puts "23andme: found genotyping-file"
+            log "23andme: found genotyping-file"
             Zip::ZipFile.open(filename) {
               |zipfile|
               zipfile.extract(entry,"#{Rails.root}/tmp/#{@genotype.fs_filename}.tsv")
               zipfile.close()
-              puts "extracted file"
+              log "extracted file"
             }
             system("mv #{Rails.root}/tmp/#{@genotype.fs_filename}.tsv #{Rails.root}/public/data/#{@genotype.fs_filename}")
-            puts "copied file"
+            log "copied file"
           end
 
         elsif @genotype.filetype == "ftdna-illumina"
-          puts "ftdna"
+          log "ftdna"
           if entry.to_s.include?("csv")
-            puts "ftdna: found genotyping-file"
+            log "ftdna: found genotyping-file"
             Zip::ZipFile.open(filename) {
                 |zipfile|
                 zipfile.extract(entry,"#{Rails.root}/tmp/#{@genotype.fs_filename}.csv")
                 zipfile.close()
-                puts "extracted file"
+                log "extracted file"
             }
             system("mv #{Rails.root}/tmp/#{@genotype.fs_filename}.csv #{Rails.root}/public/data/#{@genotype.fs_filename}")
-            puts "copied file"
+            log "copied file"
           end
             
         end
       end
       
     rescue
-      puts "nothing to unzip, seems to be a text-file in the first place"
+      log "nothing to unzip, seems to be a text-file in the first place"
     end
     
     # now that they are unzipped, check if they're actually proper files
@@ -75,27 +79,27 @@ class Preparsing
     if @genotype.filetype == "23andme"
         # first non-comment line is of length 4 after split
         if l.split("\t").length == 4
-            puts "file is 23andme and is ok!"
+            log "file is 23andme and is ok!"
             file_is_ok = true
         end
     elsif @genotype.filetype == "decodeme"
         # first line is of length 6
         if l.split(",").length == 6
             file_is_ok = true
-            puts "file is decodeme and is ok!"
+            log "file is decodeme and is ok!"
         end
     elsif @genotype.filetype == "ftdna-illumina"
         # first line is of length 4
         if l.split(",").length == 4
             file_is_ok = true
-            puts "file is ftdna and is ok!"
+            log "file is ftdna and is ok!"
         end
     end
 
     # not proper file!
     if not file_is_ok
         UserMailer.parsing_error(@genotype.user_id).deliver
-        puts "file is not ok, sending email"
+        log "file is not ok, sending email"
         # should delete the uploaded file here, leaving that for now
         # might be better to keep the file for debugging
     else
