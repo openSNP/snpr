e0e2f7fd5fb8c87eed12f1da6e1e9d5eb72d8bb7
drsnuggles <philippbay@gmail.com>
2012-09-06 10:45:45 +1000

    Added check for proper files in the preparsing-job

--- app/jobs/preparsing.rb
+++ app/jobs/preparsing.rb
@@ -45,13 +45,46 @@ class Preparsing
       puts "nothing to unzip, seems to be a text-file in the first place"
     end
     
+    # now that they are unzipped, check if they're actually proper files
     
-    system("csplit -k -f #{@genotype.id}_tmpfile -n 4 #{filename} 20000 {2000}")
-    system("mv #{@genotype.id}_tmpfile* tmp/")
-    
-    temp_files = Dir.glob("tmp/#{@genotype.id}_tmpfile*")
-    temp_files.each do |single_temp_file|
-      Resque.enqueue(Parsing, @genotype.id, single_temp_file)
+    file_is_ok = false
+    fh = File.open("#{Rails.root}/public/data/#{@genotype.fs_filename}")
+    l = fh.readline()
+    # iterate as long as there's commenting going on
+    while l.start_with?("#")
+        l = fh.readline()
+    end
+
+    if @genotype.filetype == "23andme"
+        # first non-comment line is of length 4 after split
+        if l.split("\t").length == 4
+            file_is_ok = true
+        end
+    elsif @genotype.filetype == "decodeme"
+        # first line is of length 6
+        if l.split(",").length == 6
+            file_is_ok = true
+        end
+    elsif @genotype.filetype == "ftdna-illumina"
+        # first line is of length 4
+        if l.split(",").length == 4
+            file_is_ok = true
+        end
+    end
+
+    # not proper file!
+    if not file_is_ok
+        UserMailer.parsing_error(@genotype.user_id).deliver
+        # should delete the uploaded file here, leaving that for now
+        # might be better to keep the file for debugging
+    else
+        system("csplit -k -f #{@genotype.id}_tmpfile -n 4 #{filename} 20000 {2000}")
+        system("mv #{@genotype.id}_tmpfile* tmp/")
+        
+        temp_files = Dir.glob("tmp/#{@genotype.id}_tmpfile*")
+        temp_files.each do |single_temp_file|
+        Resque.enqueue(Parsing, @genotype.id, single_temp_file)
+        end
     end
   end
 end
