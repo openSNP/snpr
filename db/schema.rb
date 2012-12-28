353fa1a66190a2f7bd3965d5b8ec9f23373f34a5
drsnuggles <philippbay@gmail.com>
2012-12-28 21:10:41 +1000

    Reintroduced the popup on phenotypes-button in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -242,9 +242,9 @@ ActiveRecord::Schema.define(:version => 20121213120010) do
     t.string   "allele_frequency"
     t.integer  "ranking"
     t.integer  "number_of_users",    :default => 0
-    t.datetime "mendeley_updated",   :default => '2012-09-18 16:32:05'
-    t.datetime "plos_updated",       :default => '2012-09-18 16:32:05'
-    t.datetime "snpedia_updated",    :default => '2012-09-18 16:32:05'
+    t.datetime "mendeley_updated",   :default => '2012-11-11 11:42:12'
+    t.datetime "plos_updated",       :default => '2012-11-11 11:42:12'
+    t.datetime "snpedia_updated",    :default => '2012-11-11 11:42:12'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
