0144d4e3df999c267faa160fb89b543087629e84
Helge Rausch <helge@rausch.io>
2013-01-13 14:53:13 +0100

    moving user csv creation to own method

--- app/jobs/zipfulldata.rb
+++ app/jobs/zipfulldata.rb
@@ -6,62 +6,32 @@ class Zipfulldata
   def self.perform(target_address)
     Rails.logger.level = 0
     Rails.logger = Logger.new("#{Rails.root}/log/zipfulldata_#{Rails.env}.log")
-    genotyping_files = []
-    users = []
 
-    Genotype.find_each do |g|
-      genotyping_files << g
-      users << User.find(g.user_id)
-    end
-    
-    # only try to create csv & zip-file if there is data at all. 
-    
-    if genotyping_files != []
+    genotypes = Genotype.includes(user: :user_phenotypes).all
+
+    # only try to create csv & zip-file if there is data at all.
+
+    if genotypes != []
       time = Time.now.utc
       time_str = time.strftime("%Y%m%d%H%M")
       time = time.to_s.gsub(":","_")
-      
+
+      csv_options = { col_sep: ';' }
+
       # only create a new file if in the current minute none has been created yet
-      
+
       if File.exists?(::Rails.root.to_s+"/public/data/zip/opensnp_datadump."+time_str+".zip") == false
-        
-        #create csv-head and writes to csv-filehandle
-        
-        csv_head = "user_id;date_of_birth;chrom_sex"
-        csv_handle = File.new(::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".csv","w")
-        phenotype_id_array = []
-    
-        Phenotype.find_each do |p|
-          phenotype_id_array << p.id
-          csv_head = csv_head + ";" + p.characteristic.gsub(";",",")
-        end
-        csv_handle.puts(csv_head)
-    
-        # create lines in csv-file for each user who has uploaded his data
-        
-        users.each do |u|
-          user_line = u.id.to_s + ";" + u.yearofbirth + ";" + u.sex
-          phenotype_id_array.each do |pid|
-            if UserPhenotype.find_by_user_id_and_phenotype_id(u.id,pid) != nil
-              user_line = user_line + ";" + UserPhenotype.find_by_user_id_and_phenotype_id(u.id,pid).variation.gsub(";",",")
-            else 
-              user_line = user_line + ";" + "-"
-            end          
-          end
-          csv_handle.puts(user_line)
-        end
-        
-        csv_handle.close
-        log "created csv-file"
-        
+
+        create_user_csv(genotypes, time_str, csv_options)
+
         # Create a file of fitbit-data for each user with fitbit-data
-        
+
         fitbit_profiles = FitbitProfile.find(:all)
         fitbit_profiles.each do |fp|
           # open handle
           fitbit_handle = File.new(::Rails.root.to_s+"/tmp/dump_user"+fp.user.id.to_s+"_fitbit_data_"+time_str.to_s+".csv","w")
           fitbit_handle.puts("date;steps;floors;weight;bmi;minutes asleep;minutes awake; times awaken; minutes until fell asleep")
-          
+
           # get all dates which have to be included in the csv
           time_array = []
           fp.fitbit_bodies.each do |fb|
@@ -73,9 +43,9 @@ class Zipfulldata
           fp.fitbit_activities.each do |fa|
             time_array << fa.date_logged
           end
-          
+
           time_array = time_array.uniq.sort
-          
+
           time_array.each do |d|
             line = d + ";"
             activity = fp.fitbit_activities.find_by_date_logged(d)
@@ -84,14 +54,14 @@ class Zipfulldata
             else
               line = line + activity.steps + ";" + activity.floors+ ";"
             end
-            
+
             body = fp.fitbit_bodies.find_by_date_logged(d)
             if body == nil
               line = line + "-;-;"
             else
               line = line + body.weight + ";" + body.bmi + ";"
             end
-            
+
             sleep = fp.fitbit_sleeps.find_by_date_logged(d)
             if sleep == nil
               line = line + "-;-;-;-;"
@@ -105,7 +75,7 @@ class Zipfulldata
         end
 
         # picture phenotype zipping comes here
-        
+
         # make a CSV describing all of them - which filename is for which user's phenotype
         csv_head = "user_id;date_of_birth;chrom_sex"
         csv_handle = File.new(::Rails.root.to_s+"/tmp/picture_dump"+time_str.to_s+".csv","w")
@@ -116,9 +86,9 @@ class Zipfulldata
           csv_head = csv_head + ";" + p.characteristic.gsub(";",",")
         end
         csv_handle.puts(csv_head)
-    
+
         # create lines in csv-file for each user who has uploaded his data
-        
+
         list_of_pics = [] # need this for the zip-file-later
 
         User.all.each do |u|
@@ -138,14 +108,14 @@ class Zipfulldata
 
               list_of_pics << picture
               user_line = user_line + ";" + picture.id.to_s + "." +  filetype
-            else 
+            else
               user_line = user_line + ";" + "-"
             end
           end
           log "Putting a line into CSV"
           csv_handle.puts(user_line)
         end
-        
+
         csv_handle.close
         log "created picture handle csv-file"
 
@@ -167,8 +137,8 @@ class Zipfulldata
         end
 
         log "created picture zip file"
-        
-        # make a README containing time of zip - this way, users can compare with page-status 
+
+        # make a README containing time of zip - this way, users can compare with page-status
         # and see how old the data is
         readme_handle = File.new(::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".txt","w")
         phenotype_count = Phenotype.count
@@ -177,16 +147,16 @@ class Zipfulldata
         readme_handle.puts("This archive was generated on "+time.to_s+" UTC. It contains "+phenotype_count.to_s+" phenotypes, "+genotype_count.to_s+" genotypes and " + picture_count.to_s + " picture phenotypes.")
         readme_handle.puts("Thanks for using openSNP!")
         readme_handle.close
-    
-        # zip up everything (csv + all genotypings + pics-zip + pics-csv + readme) 
-        
+
+        # zip up everything (csv + all genotypings + pics-zip + pics-csv + readme)
+
         zipname = "/data/zip/opensnp_datadump."+time_str+".zip"
         Zip::ZipFile.open(::Rails.root.to_s+"/public/"+zipname, Zip::ZipFile::CREATE) do |zipfile|
           zipfile.add("picture_phenotypes_" + time_str.to_s + ".csv", ::Rails.root.to_s+"/tmp/picture_dump"+time_str.to_s+".csv")
           zipfile.add("picture_phenotypes_" + time_str.to_s + "_all_pics.zip", ::Rails.root.to_s + "/public/" + pic_zipname)
-          zipfile.add("phenotypes_"+time_str.to_s+".csv",::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".csv") 
+          zipfile.add("phenotypes_"+time_str.to_s+".csv",::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".csv")
           zipfile.add("readme.txt",::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".txt")
-          genotyping_files.each do |gen_file|
+          genotypes.each do |gen_file|
             yob = gen_file.user.yearofbirth
             sex = gen_file.user.sex
             if yob == "rather not say"
@@ -197,11 +167,11 @@ class Zipfulldata
             end
             zipfile.add("user"+gen_file.user_id.to_s+"_file"+gen_file.id.to_s+"_yearofbirth_"+yob+"_sex_"+sex+"."+gen_file.filetype+".txt", ::Rails.root.to_s+"/public/data/"+ gen_file.fs_filename)
           end
-          
+
           fitbit_profiles.each do |fp|
             zipfile.add("user"+fp.user.id.to_s+"_fitbit_data_"+time_str.to_s+".csv",::Rails.root.to_s+"/tmp/dump_user"+fp.user.id.to_s+"_fitbit_data_"+time_str.to_s+".csv")
           end
-          
+
         end
         if FileLink.find_by_description("all genotypes and phenotypes archive") == nil
             filelink = FileLink.new(:description => "all genotypes and phenotypes archive", :url => zipname)
@@ -209,7 +179,7 @@ class Zipfulldata
         else
             FileLink.find_by_description("all genotypes and phenotypes archive").update_attributes(:url => zipname)
         end
-        
+
         File.delete(::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".csv")
         File.delete(::Rails.root.to_s+"/tmp/dump"+time_str.to_s+".txt")
         fitbit_profiles.each do |fp|
@@ -217,7 +187,7 @@ class Zipfulldata
         end
         log "created zip-file"
       end
-      
+
       # make sure the file-permissions of the resulting zip-file are okay and send mail
       system("chmod 777 "+::Rails.root.to_s+"/public/data/zip/opensnp_datadump."+time_str+".zip")
       UserMailer.dump(target_address,"/data/zip/opensnp_datadump."+time_str+".zip").deliver
@@ -225,6 +195,32 @@ class Zipfulldata
     else
       UserMailer.no_dump(target_address).deliver
     end
+  end
+
+  def self.create_user_csv(genotypes, time_str, csv_options)
+    phenotypes = Phenotype.all
+    csv_head = %w(user_id date_of_birth chrom_sex)
+    csv_head.concat(phenotypes.map(&:characteristic))
+
+    CSV.open("#{Rails.root}/tmp/dump#{time_str}.csv", "w", csv_options) do |csv|
+      csv << csv_head
+
+      # create lines in csv-file for each user who has uploaded his data
+      genotypes.each do |genotype|
+        user = genotype.user
+        row = [user.id, user.yearofbirth, user.sex]
+
+        phenotypes.each do |phenotype|
+          if up = user.user_phenotypes.where(phenotype_id: phenotype.id).first
+            row << up.variation
+          else
+            row << "-"
+          end
+        end
+        csv << row
+      end
+    end
+    log "created csv-file"
   end
 
   def self.log msg
