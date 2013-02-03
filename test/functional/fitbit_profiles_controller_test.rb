9dc90e931916df80887d18a1fcb7d7ce3b018d01
Helge Rausch <helge@rausch.io>
2013-02-03 13:57:45 +0100

    adding a tiny test

---
+++ test/functional/fitbit_profiles_controller_test.rb
@@ -0,0 +1,15 @@
+require_relative '../test_helper'
+
+class FitbitProfilesControllerTest < ActionController::TestCase
+  context "Fitbit profiles" do
+    setup do
+      @user = FactoryGirl.create(:user)
+      @fitbit_profile = FactoryGirl.create(:fitbit_profile, user: @user)
+    end
+
+    should "show up" do
+      get 'show', id: @fitbit_profile.id
+      assert_response :ok
+    end
+  end
+end
