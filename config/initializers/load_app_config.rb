7473e2f4a08e315948cdac9db9dbe24ec0043ad7
drsnuggles <philippbay@gmail.com>
2013-03-22 09:31:20 +1000

    Updated gems slightly. Hopefully working workaround for missing logs in production.

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,2 +1,6 @@
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
 APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+
+
+Rails.logger.instance_variable_get(:@logger).instance_variable_get(:@log_dest).sync = true if Rails.logger
+
