9b045a251ccfa50c7e92f859903a127a100a77e5
Helge Rausch <helge@rausch.io>
2013-08-31 12:31:11 +0200

    stack the munin graphs for sidekiq
    also handle sidekiq not running

--- script/munin/sidekiq_workers.sh
+++ script/munin/sidekiq_workers.sh
@@ -7,7 +7,9 @@ graph_title Sidekiq workers
 graph_vlabel #
 graph_category opensnp.org
 idle.label idle
+idle.draw AREA
 busy.label busy
+busy.draw STACK
 EOM
 	exit 0;;
 esac
@@ -16,6 +18,8 @@ esac
 ps=$(ps -eo command | grep sidekiq | grep busy | cut -d '[' -f 2)
 busy=$(echo $ps | cut -d ' ' -f 1)
 total=$(echo $ps | cut -d ' ' -f 3)
+[ -n "${busy}" ] || busy=0
+[ -n "${total}" ] || total=0
 idle=$(($total-$busy))
 
 echo "busy.value ${busy}"
