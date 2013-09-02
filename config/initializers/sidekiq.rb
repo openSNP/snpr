1f056d71e9b4a4b5f142b32604ab6c89e8061792
Helge Rausch <helge@rausch.io>
2013-09-02 09:39:13 +0200

    configurable username and pq for sidekiq web ui

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,9 @@ require 'sidekiq'
 require 'sidekiq/web'
 require 'sidekiq/limit_fetch'
 
+
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-      [user, password] == ["admin", "password"]
+  u = (APP_CONFIG[:sidekiq].try(:[], "user") || "admin")
+  p = (APP_CONFIG[:sidekiq].try(:[], "password") || "password")
+  [user, password] == [u, p]
 end
