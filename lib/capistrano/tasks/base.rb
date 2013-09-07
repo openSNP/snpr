273a006d5b53bfe4e1f876b7eb380d270b0cf52d
Helge Rausch <helge@rausch.io>
2013-09-07 11:15:29 +0200

    set symlink to data dir on deploy

--- lib/capistrano/tasks/base.rb
+++ lib/capistrano/tasks/base.rb
@@ -7,6 +7,7 @@ namespace :deploy do
     ln("#{shared_path}/config/mail_password.txt", "#{release_path}/mail_password.txt")
     ln("#{shared_path}/config/key_mendeley.txt", "#{release_path}/key_mendeley.txt")
     ln("#{shared_path}/config/key_plos.txt", "#{release_path}/key_plos.txt")
+    ln("#{shared_path}/data", "#{release_path}/public/data")
     mkdir("#{shared_path}/assets")
     ln("#{shared_path}/assets", "#{release_path}/public/assets")
   end
