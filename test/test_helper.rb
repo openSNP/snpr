1e67cd43c70614abd75a59704fe48f26331803ab
Helge Rausch <helge@rausch.io>
2013-12-08 14:03:15 +0100

    no simplecov for travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
-require 'simplecov' unless ENV['CI']
-SimpleCov.start('rails')
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start('rails')
+end
 require File.expand_path('../../config/environment', __FILE__)
 require "test/unit"
 require "shoulda-context"
