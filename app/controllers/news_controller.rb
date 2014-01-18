7672773a9de00c95a6fc22af2e944bb0a7bb4b0a
Philipp Bayer <philippbay@gmail.com>
2014-01-18 12:22:35 +1000

    Removes unneeded function in news-controller, adds one test for news-controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,41 +1,32 @@
 class NewsController < ApplicationController
 
-	def index
-		@title = "News"
-		@new_genotypes = Genotype.all(:order => "created_at DESC", :limit => 20)
-		@new_users = User.all(:order => "created_at DESC", :limit => 20)
-		@new_phenotypes = Phenotype.all(:order => "created_at DESC", :limit => 20)
-		@new_phenotype_comments = PhenotypeComment.all(:order => "created_at DESC", :limit => 20)
-		@new_snp_comments = SnpComment.all(:order => "created_at DESC", :limit => 20)
-		
-		@newest_plos_paper = PlosPaper.all(:order => "created_at DESC", :limit => 20)
+  def index
+    @title = "News"
+    @new_genotypes = Genotype.all(:order => "created_at DESC", :limit => 20)
+    @new_users = User.all(:order => "created_at DESC", :limit => 20)
+    @new_phenotypes = Phenotype.all(:order => "created_at DESC", :limit => 20)
+    @new_phenotype_comments = PhenotypeComment.all(:order => "created_at DESC", :limit => 20)
+    @new_snp_comments = SnpComment.all(:order => "created_at DESC", :limit => 20)
+
+    @newest_plos_paper = PlosPaper.all(:order => "created_at DESC", :limit => 20)
     @newest_mendeley_paper = MendeleyPaper.all(:order => "created_at DESC", :limit => 20)
-    
+
     @newest_paper = @newest_mendeley_paper | @newest_plos_paper
     @newest_paper.sort! { |a,b| b.created_at <=> a.created_at }
 
-		respond_to do |format|
-			format.html
-		end
-	end
-	
-	def test
-	  @title = "foo"
-	  @new_genotypes = Genotype.all(:order => "created_at DESC", :limit => 20)
-	  
-	  respond_to do |format|
-	    format.html
-    end 
+    respond_to do |format|
+      format.html
+    end
   end
-
+    
   def paper_rss
     @newest_plos_paper = PlosPaper.all(:order => "created_at DESC", :limit => 20)
     @newest_mendeley_paper = MendeleyPaper.all(:order => "created_at DESC", :limit => 20)
-    
+
     @newest_paper = @newest_mendeley_paper | @newest_plos_paper
     @newest_paper.sort! { |a,b| b.created_at <=> a.created_at }
-    
-      render :action => "paper_rss", :layout => false
+
+    render :action => "paper_rss", :layout => false
   end
 
 end
