57ec02d7bcc52c416209d86f149a01be63f56e8e
Helge Rausch <helge@rausch.io>
2014-06-08 12:46:27 +0200

    Update RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'sidekiq/testing'
 require 'factory_girl_rails'
 require 'sunspot_test/rspec'
