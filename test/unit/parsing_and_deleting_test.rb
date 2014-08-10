bfb85036318654bd7cd3170a1e2bb79c32064d25
Helge Rausch <helge@rausch.io>
2014-08-10 12:40:22 +0200

    Fix test

--- test/unit/parsing_and_deleting_test.rb
+++ test/unit/parsing_and_deleting_test.rb
@@ -45,7 +45,6 @@ class ParsingTest < ActiveSupport::TestCase
       assert_equal expected_genotypes, user_snp_genotypes
       user_snps.each do |s|
         assert_equal @genotype_23andme.id, s.genotype_id
-        assert_equal @genotype_23andme.user.id, s.user_id
         assert Snp.pluck(:name).include?(s.snp_name)
       end
     end
@@ -87,7 +86,6 @@ class ParsingTest < ActiveSupport::TestCase
       assert_equal expected_genotypes, user_snp_genotypes
       user_snps.each do |s|
         assert_equal @genotype_decodeme.id, s.genotype_id
-        assert_equal @genotype_decodeme.user.id, s.user_id
         assert Snp.pluck(:name).include?(s.snp_name)
       end
     end
