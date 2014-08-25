c2580d67466f6f4e1842f3f2e2b5c7e6c01cec0a
Philipp Bayer <philippbay@gmail.com>
2014-08-25 11:46:43 +1000

    Used wrong format

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,7 @@
 # This is the config to talk to opensnperr.herokuapp.com
 Airbrake.configure do |config|
-  config.api_key = APP_CONFIG['ERRBIT']['API_KEY']
-  config.host    = APP_CONFIG['ERRBIT']['HOST']
+  config.api_key = APP_CONFIG[:errbit][:api_key]
+  config.host    = APP_CONFIG[:errbit][:host]
   config.port    = 80
   config.secure  = config.port == 443
   config.environment_name = Rails.env.production? ? `hostname` : Rails.env
