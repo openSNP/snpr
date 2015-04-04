7de5e36c3b574201784a6c65f28eb9b094e309d9
Philipp Bayer <philippbay@gmail.com>
2015-04-04 22:32:31 +0200

    Adds production log level to get rid of deprecation warning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Snpr::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
