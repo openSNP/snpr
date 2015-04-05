cd9f1b9c8f00aac03b33ce6bae93a8d923f9e512
Helge Rausch <helge@rausch.io>
2015-04-05 14:59:41 +0200

    Clean up a little...

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,86 +1,69 @@
 class MessagesController < ApplicationController
-
   before_filter :require_user
-  before_filter :require_owner, only: [ :show, :destroy ]
+  before_filter :require_owner, only: [:show, :destroy]
 
   def new
-    #create the message
+    @title = 'New message'
+
     @message = Message.new
-    @title = "New message"
     @users = User.all
-    # ideally, we would kick out the current_user however, this generates crashes when there are only two users (function "map" doesn't work on a single object)
-    if params[:message] 
+    if params[:message]
       @answering = Message.find_by_id(params[:message])
-      @allowed = true
-      if @answering.from_id != current_user.id and @answering.to_id != current_user.id 
-        @allowed = false
+      if @answering.from_id != current_user.id && @answering.to_id != current_user.id
+        @answering = nil
       end
-    end
-    respond_to do |format|
-      format.html
-      format.xml { render :xml => @message }
     end
   end
 
   def create
     @message = Message.new(message_params)
-    @message.user_id = current_user.id
-    @message.user_has_seen = true
-    @message.from_id = current_user.id
-    @message.sent = true
 
-    if params[:user] != nil
-      @message.to_id = params[:user][:id]
-    end
-
-    if @message.save and @message.send_message(@message.from_id, @message.to_id)
-      flash[:notice] = "Message sent!"
-      redirect_to "/users/" + current_user.id.to_s + "#messages"
+    if @message.save && @message.send_message(@message.from_id, @message.to_id)
+      flash[:notice] = 'Message sent!'
+      redirect_to "/users/#{current_user.id}#messages"
     else
-      respond_to do |format|
-        format.html { render :action => "new" }
-        format.xml { render :xml => @message.errors, :status => :unprocessable_entity }
-      end
+      render action: 'new'
     end
   end
 
   def show
     @message = Message.find_by_id(params[:id])
-    if User.find_by_id(@message.from_id) != nil
+    if !User.find_by_id(@message.from_id).nil?
       @from = User.find_by_id(@message.from_id)
     else
-      @from = "Deleted User"
+      @from = 'Deleted User'
     end
 
-    if User.find_by_id(@message.to_id) != nil 
+    if !User.find_by_id(@message.to_id).nil?
       @to = User.find_by_id(@message.to_id)
     else
-      @to = "Deleted User"
+      @to = 'Deleted User'
     end
-    @message.update_attributes :user_has_seen => true
+    @message.update_attributes user_has_seen: true
   end
 
   def destroy
-    message = Message.find_by_id(params[:id])
-    if message.destroy
-      flash[:notice] = "Message deleted."
-      redirect_to "/users/" + current_user.id.to_s + "#messages"
+    message = Message.where(from_id: current_user.id, id: params[:id]).first
+    if message
+      message.destroy
+      flash[:notice] = 'Message deleted.'
+      redirect_to '/users/' + current_user.id.to_s + '#messages'
+    else
+      render text: 'Unauthorized', status: :unauthorized
     end
   end
 
   private
 
   def require_owner
-    unless current_user.id == Message.find_by_id(params[:id]).user_id
-      store_location
-      if current_user
-        flash[:warning] = "Oops! Thats none of your business"
-        redirect_to :controller => "users", :action => "show", :id => current_user.id 
-      else
-        flash[:notice] = "You need to be logged in"
-        redirect_to "/signin"
-      end
-      return false
+    return if current_user.id == Message.find(params[:id]).user_id
+    store_location
+    if current_user
+      flash[:warning] = 'Oops! Thats none of your business'
+      redirect_to controller: 'users', action: 'show', id: current_user.id
+    else
+      flash[:notice] = 'You need to be logged in'
+      redirect_to '/signin'
     end
   end
 
