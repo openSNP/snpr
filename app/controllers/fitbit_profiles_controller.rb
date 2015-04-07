e9b8c02bda05716a89d2253db9f71999b032c0d3
Helge Rausch <helge@rausch.io>
2015-04-07 07:53:57 +0200

    Fix fitbit profiles page

--- app/controllers/fitbit_profiles_controller.rb
+++ app/controllers/fitbit_profiles_controller.rb
@@ -6,14 +6,12 @@ class FitbitProfilesController < ApplicationController
 
   def index
     @title = "Listing all connected Fitbit accounts"
-    @fitbit = FitbitProfile.order(sort_column + " " + sort_direction)
-    @fitbit_paginate = @fitbit.paginate(:page => params[:page],:per_page => 20)
+    @fitbit_profiles = FitbitProfile
+      .includes(:user)
+      .order("#{sort_column} #{sort_direction}")
+      .paginate(page: params[:page], per_page: 20)
     @filelink = FileLink.
       where(description: "all genotypes and phenotypes archive").first.try(:url)
-    respond_to do |format|
-      format.html
-      format.xml 
-    end
   end
   
   def show
