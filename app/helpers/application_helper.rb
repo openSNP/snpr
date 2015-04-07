709f5420f0f17219924185d4d38245fae1e208f2
Helge Rausch <helge@rausch.io>
2015-04-07 09:01:45 +0200

    Move table row sequence numbering to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-	#def mark_required(object, attribute)
-	#	"*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator
-	#end
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
@@ -12,22 +9,26 @@ module ApplicationHelper
   class BootstrapLinkRenderer < ::WillPaginate::ActionView::LinkRenderer
     protected
 
-      def html_container(html)
-        tag :div, tag(:ul, html), container_attributes
-      end
-
-      def page_number(page)
-        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
-      end
-      def gap
-        tag :li, link('&hellip;'.html_safe, '#'), :class => 'disabled'
-      end
-      def previous_or_next_page(page, text, classname)
-        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
-      end
+    def html_container(html)
+      tag :div, tag(:ul, html), container_attributes
     end
 
-    def page_navigation_links(pages)
-      will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe)
+    def page_number(page)
+      tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
     end
+    def gap
+      tag :li, link('&hellip;'.html_safe, '#'), :class => 'disabled'
+    end
+    def previous_or_next_page(page, text, classname)
+      tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+    end
+  end
+
+  def page_navigation_links(pages)
+    will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe)
+  end
+
+  def table_row_sequence_number(paginated, current_page_index)
+    paginated.per_page * (paginated.current_page - 1) + current_page_index + 1
+  end
 end
