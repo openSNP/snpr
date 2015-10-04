5f337a6b5906042b04a2df25b690d1f2b548fc57
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 14:05:55 -0500

    put an underscore in front of evaluator

--- spec/factories/fitbit_profiles.rb
+++ spec/factories/fitbit_profiles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :fitbit_profile do
-    after(:create) do |fp, evaluator|
+    after(:create) do |fp, _evaluator|
       params = { fitbit_profile: fp, date_logged: '2013-01-14' }
       FactoryGirl.create(:fitbit_body, params)
       FactoryGirl.create(:fitbit_sleep, params)
