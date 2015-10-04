410216d5105286b04380a6461b57364aca93c7b7
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 13:20:12 -0500

    clean up from rubocop suggestions

--- spec/factories/fitbit_sleeps.rb
+++ spec/factories/fitbit_sleeps.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :fitbit_sleep do
-    minutes_asleep  480
-    minutes_awake    10
+    minutes_asleep 480
+    minutes_awake 10
     number_awakenings 1
     minutes_to_sleep 10
   end
