4e2e0d4008060d622325a77cb511630c214fbc83
Philipp Bayer <philippbay@gmail.com>
2016-02-11 21:37:25 +0800

    Adds a barebones test

---
+++ spec/workers/plos_details_spec.rb
@@ -0,0 +1,13 @@
+describe PlosDetails do
+  let(:plos_paper) do
+    create(:plos_paper, id: 1, reader: 0, doi: "10.1371/journal.pone.0089204")
+  end
+  let(:job) {PlosDetails.new}
+
+  it "updates the view count" do
+    before = plos_paper.reader
+    job.perform plos_paper.id
+    expect(plos_paper.reader).not_to eq(before)
+  end
+
+end
