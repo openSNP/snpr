12aea857c239bbfcacd783ecbdf54b9b161b4840
Philipp Bayer <philippbay@gmail.com>
2016-02-11 22:24:40 +0800

    Test does not update viewer count yet - going to bed for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'sidekiq/testing'
 require 'factory_girl_rails'
 require 'pry-rails' unless ENV['CI']
 require 'authlogic/test_case'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
--- spec/workers/plos_details_spec.rb
+++ spec/workers/plos_details_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper.rb'
+
 describe PlosDetails do
   let(:plos_paper) do
     create(:plos_paper, id: 1, reader: 0, doi: '10.1371/journal.pone.0089204')
@@ -6,7 +8,13 @@ describe PlosDetails do
 
   it 'updates the view count' do
     before = plos_paper.reader
+
+    response = double('response', body: '{"views":23}')
+    expect(Net::HTTP).to receive(:get_response).
+        with(URI 'http://alm.plos.org/api/v3/articles/10.1371/journal.pone.0089204?api_key=foo').
+        and_return(response)
+    
     job.perform plos_paper.id
-    expect(plos_paper.reader).not_to eq(before)
+    expect(plos_paper.reader).to eq(23)
   end
 end
