45f172bf2ecb3ad543e694375c8a36642f13b348
Philipp Bayer <philippbay@gmail.com>
2016-02-12 23:14:38 +0800

    Implements Helge's recommendations

--- spec/integration/plos_details_spec.rb
+++ spec/integration/plos_details_spec.rb
@@ -4,15 +4,9 @@ describe PlosDetails, :vcr do
   let(:plos_paper) do
     create(:plos_paper, id: 1, reader: 0, doi: '10.1371/journal.pone.0089204')
   end
-  let(:job) {PlosDetails.new}
 
   it 'updates the view count' do
-    before = plos_paper.reader
-
-    VCR.use_cassette 'plos_detail' do
-      job.perform plos_paper.id
-    end
-    expect(plos_paper.reload.reader).not_to eq(before)
+    subject.perform plos_paper.id
     expect(plos_paper.reload.reader).to eq(5047) # that's a lot of openSNP readers!
   end
 end
