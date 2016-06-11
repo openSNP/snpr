2e0048805350bd3e5cfdde0617e656e1966bff8f
Vivek Rai <vivekrai.iitkgp@gmail.com>
2016-06-11 15:11:27 -0700

    Add worker script logic for scoring/ranking
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

--- app/workers/phenotype_snp.rb
+++ app/workers/phenotype_snp.rb
@@ -4,56 +4,55 @@ class SnpToPhenotype
   attr_reader :snp, :client
 
   def perform(snp_id)
-    # could possible use the max_age limit here
-    @snp = Snp.find(snp_id)
-    if snp && valid_snp_names.include?(snp.name)
-      score_pehnotype
-    end
+    # could possibly use the max_age limit here
+    @psnp = PhenotypeSnp.where(:snp_id => snp_id)
+    @characteristics = Phenotypes.all.map { |x| x.characteristic }
+    @papers_count = 0
+    psnp.map { |x| score_phenotype(x) }
   end
 
-  def score_phenotype
+  def score_phenotype (psnp)
     # TODO:
-    # - fetch papers metadata corresponding to the SNP
     # - fetch all the stored phenotypes
+    # (available in @characteristics)
+
+    # - fetch papers metadata corresponding to the SNP
+    plos = score_paper(:plos_papers, 2)
+    snpedia = score_paper(:snpedia_papers, 5)
+    pgp = score_paper(:pgp_annotation, 2)
+    genomegov = score_paper(:genome_gov_papers, 2)
+    mendeley = score_paper(:mendeley_papers, 1)
+
+    all_scores = [snpedia, pgp, genomegov, mendeley].reduce(plos) do |x, y|
+      x.merge(y) do |k, v1, v2|
+        v1 + v2
+      end
+    end
+
+    all_scores.sort_by! {|k, v| v}
+
+    all_scores.take(10) do |k, v|
+      ph = Phenotype.find_by_characteristic(k)
+      PhenotypeSnp.find_or_initialize_by(:snp_id => psnp)
+                  .update_attributes!(:phenotype_id => ph, :score => v)
+    end
+  end
+
+  def score_paper(paper_type, weight)
     # - search for each phenotype one by one in the papers' metadata
-    # - maintain a hash of the phenotypes along with score
-    # - return entire hash
+    score = {}
 
-  end
+    papers = psnp.public_send(paper_type)
+    papers_count += papers.length
 
-  def top_phenotypes(limit)
-    # - return top phenotypes based on score from the hash
-  end
-
-  def perform_search
-    # return an array of page-titles
-    pages = client.list("#{snp.name}(")
-    snpedia_updated = false
-    (pages || []).each do |page|
-      next unless page.include?('(')
-      url = "http://www.snpedia.com/index.php/#{page}"
-      # revision returns an int which grows with changes
-      rev_id = client.revision(page).to_i
-      snpedia_paper = SnpediaPaper.find_or_initialize_by(url: url)
-      next if snpedia_paper.persisted? && snpedia_paper.revision == rev_id
-      to_parse = client.get(page)
-      next if to_parse.to_s.include?('#REDIRECT')
-      /summary=(?<summary>.*)\}\}/m =~ to_parse
-      snpedia_paper.update_attributes!(
-        url: url, summary: summary, revision: rev_id)
-      snpedia_paper.snps << snp unless snpedia_paper.snps.include? snp
-      snpedia_updated = true
+    characteristics.each do |chr|
+      papers.each do |p|
+        if p.title.downcase.include? chr.douwncase
+          score[chr.downcase] = weight + score[chr.downcase] || 0
+        end
+      end
+      score[chr.downcase] /= papers.length
     end
-    snp.snpedia_updated! if snpedia_updated
-    if Rails.env == 'production'
-      # Increase this value if the following error keeps on showing up
-      # 'MediaWiki::APIError: API error: code 'internal_api_error_DBConnectionError', 
-      # info 'Exception Caught: DB connection error: Too many connections'
-      sleep(10)
-    end
-  end
-
-  def valid_snp_names
-    Marshal.load(File.read(Rails.root.join('marshalled_snpedia_array')))
+    score
   end
 end
