8c31367e9a5fd45dc446d83dd5be6a5617264f92
Vivek Rai <vivekrai.iitkgp@gmail.com>
2016-06-12 21:02:06 -0700

    Add spec files skeleton
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

---
+++ spec/models/phenotype_snp_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe PhenotypeSnp do
+  subject do
+  end
+
+  describe 'snp' do
+    it 'has at least one phenotype' do
+    end
+
+    it 'has a non-negative score' do
+    end
+  end
+
+end
---
+++ spec/workers/link_phenotype_snp_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe LinkSnpPhenotype do
+  subject do
+  end
+
+  describe 'scoring' do
+    it 'returns a non-negative score for a SNP' do
+    end
+
+    it 'returns a non-negative count of matched references for a SNP' do
+    end
+  end
+
+  describe 'phenotype matching' do
+    it 'returns a non-empty list of phenotype for a SNP' do
+    end
+
+    it 'returns no more than 10 phenotypes for a SNP' do
+    end
+  end
+end
