6b99d6942fd4d96b22994594aa79f9c78776cb90
Helge Rausch <helge@rausch.io>
2016-06-19 12:39:36 +0200

    Add method

--- app/workers/update_papers.rb
+++ app/workers/update_papers.rb
@@ -11,13 +11,17 @@ class UpdatePapers
 
   def perform
     PAPER_UPDATED_COLUMNS.each do |worker, column|
-      Snp.where("#{column} < ?", max_age).pluck(:id).each do |snp_id|
+      snp_ids(column).each do |snp_id|
         worker.perform_async(snp_id)
       end
     end
   end
 
   private
+
+  def snp_ids(column)
+    Snp.where("#{column} < ?", max_age).pluck(:id)
+  end
 
   def max_age
     MAX_AGE.ago
