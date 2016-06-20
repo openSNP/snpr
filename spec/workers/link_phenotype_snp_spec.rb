4267222faaeb7ffabecd7243c486d15bd015eeb6
Vivek Rai <vivekrai.iitkgp@gmail.com>
2016-06-19 23:56:33 -0700

    Expand spec skeleton
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

--- spec/workers/link_phenotype_snp_spec.rb
+++ spec/workers/link_phenotype_snp_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe LinkSnpPhenotype do
 
     it 'returns a non-negative count of matched references for a SNP' do
     end
+
+    it 'uses a consistent scoring scheme' do
+    end
   end
 
   describe 'phenotype matching' do
@@ -15,6 +18,11 @@ RSpec.describe LinkSnpPhenotype do
     end
 
     it 'returns no more than 10 phenotypes for a SNP' do
+    end
+  end
+
+  describe 'update phenotypes' do
+    it 'updates phenotype_snp table if more than MAX_AGE old' do
     end
   end
 end
