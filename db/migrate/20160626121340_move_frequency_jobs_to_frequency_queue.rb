105515efa99a9cb675eaec543bbb179dcc7c1de8
Helge Rausch <helge@rausch.io>
2016-06-26 14:21:52 +0200

    Move Frequency jobs to frequency queue
    In order to not block the parsing jobs, move the frequency jobs to a
    different queue.

---
+++ db/migrate/20160626121340_move_frequency_jobs_to_frequency_queue.rb
@@ -0,0 +1,10 @@
+class MoveFrequencyJobsToFrequencyQueue < ActiveRecord::Migration
+  def change
+    Sidekiq::Queue.new('user_snps').each do |job|
+      if job.klass == 'Frequency'
+        Frequency.perform_async(*job.args)
+        job.delete
+      end
+    end
+  end
+end
