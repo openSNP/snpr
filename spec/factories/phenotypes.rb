0dfb4894dbbfc0d0a3a7413f2b8e309ef9fcfe64
Austin Davis-Richardson <harekrishna@gmail.com>
2016-07-17 22:32:40 -0700

    adds specs for phenotypes json API

--- spec/factories/phenotypes.rb
+++ spec/factories/phenotypes.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :phenotype do
     characteristic 'Penis length'
+
+    factory :phenotype_with_users do
+      after :create do |t|
+        3.times { create(:user_phenotype, phenotype: t) }
+      end
+    end
   end
 end
