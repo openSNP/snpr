c6ea91eed0f57a7d5158a16a04148b1058016ace
Helge Rausch <helge@rausch.io>
2016-08-07 11:36:45 +0200

    Improve test

--- spec/integration/phenotype_number_of_users_spec.rb
+++ spec/integration/phenotype_number_of_users_spec.rb
@@ -1,22 +1,38 @@
 RSpec.describe Phenotype do
-  let(:phenotype) { create(:phenotype) }
+  let!(:phenotype) { create(:phenotype) }
   subject { phenotype.number_of_users }
 
-  before do
-    create(:user_phenotype, phenotype: phenotype)
-    create(:user_phenotype, phenotype: phenotype)
-    create(:user_phenotype, phenotype: phenotype)
-  end
+  context 'with users' do
+    before do
+      create(:user_phenotype, phenotype: phenotype)
+      create(:user_phenotype, phenotype: phenotype)
+      create(:user_phenotype, phenotype: phenotype)
+    end
 
-  context 'without number_of_users_column' do
-    it 'returns the number of users' do
-      expect(phenotype.number_of_users).to eq(3)
+    context 'without number_of_users_column' do
+      it 'returns the number of users' do
+        expect(phenotype.number_of_users).to eq(3)
+      end
+    end
+
+    context 'with number_of_users_column' do
+      it 'returns the number of users' do
+        expect(described_class.with_number_of_users.first.number_of_users).to eq(3)
+      end
     end
   end
 
-  context 'with number_of_users_column' do
-    it 'returns the number of users' do
-      expect(described_class.with_number_of_users.first.number_of_users).to eq(3)
+  context 'without users' do
+    context 'without number_of_users_column' do
+      it 'returns the number of users' do
+        expect(phenotype.number_of_users).to eq(0)
+      end
+    end
+
+    context 'with number_of_users_column' do
+      it 'returns 0' do
+        expect(described_class.with_number_of_users.first.number_of_users).to eq(0)
+      end
     end
   end
 end
