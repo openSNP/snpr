948f836b0ec56feeae8eb84d1e17e80de810b98b
Helge Rausch <helge@rausch.io>
2016-08-07 12:40:51 +0200

    More tests

--- spec/features/entering_new_phenotypes_spec.rb
+++ spec/features/entering_new_phenotypes_spec.rb
@@ -60,6 +60,65 @@ RSpec.feature 'Entering new phenotypes' do
       click_on('Create Phenotype')
       expect(page).not_to have_content('Congratulations!')
     end
+
+    context 'the phenotype exists' do
+      let!(:phenotype) { create(:phenotype, characteristic: 'Eye count') }
+
+      scenario 'the user enters a phenotype' do
+        visit('/phenotypes/new')
+
+        fill_in('Characteristic', with: 'Eye count')
+        fill_in('Description', with: 'How many eyes do you have?')
+        fill_in('Variation', with: 10)
+        click_on('Create Phenotype')
+
+        expect(page).to have_content('Phenotype successfully created.')
+        expect(page).to have_content(
+          "Congratulations! You've unlocked an achievement: Entered first phenotype"
+        )
+
+        expect(page.current_path).to eq("/users/#{user.id}")
+
+        expect(Phenotype.count).to eq(1)
+        expect(UserPhenotype.find_by(phenotype: phenotype, user: user).variation).to eq('10')
+
+        user.reload
+        expect(user.phenotype_creation_counter).to eq(0)
+        expect(user.achievements.map(&:award))
+          .to match_array(['Entered first phenotype'])
+
+        expect(ActionMailer::Base.deliveries.count).to eq(0)
+      end
+
+      context 'and the user phenotype exists as well' do
+        let!(:user_phenotype) { create(:user_phenotype, phenotype: phenotype, user: user) }
+
+        scenario 'the user enters a phenotype' do
+          visit('/phenotypes/new')
+
+          fill_in('Characteristic', with: 'Eye count')
+          fill_in('Description', with: 'How many eyes do you have?')
+          fill_in('Variation', with: 10)
+          click_on('Create Phenotype')
+
+          expect(page).not_to have_content('Phenotype successfully created.')
+          expect(page).to have_content('phenotype has already been entered')
+          expect(page).not_to have_content(
+            "Congratulations! You've unlocked an achievement: Entered first phenotype"
+          )
+
+          expect(page.current_path).to eq("/phenotypes")
+
+          expect(Phenotype.count).to eq(1)
+          expect(UserPhenotype.count).to eq(1)
+
+          user.reload
+          expect(user.phenotype_creation_counter).to eq(0)
+          expect(user.achievements.count).to eq(0)
+          expect(ActionMailer::Base.deliveries.count).to eq(0)
+        end
+      end
+    end
   end
 
   context 'a not signed-in user', :js do
