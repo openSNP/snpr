ae6e6a62219361b8ab80097610dc27101a9abae0
Helge Rausch <helge@rausch.io>
2016-08-08 14:12:19 +0200

    Fix VCR config

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -2,5 +2,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.ignore_hosts %w(127.0.0.1 codeclimate.com)
+  c.ignore_hosts '127.0.0.1', 'codeclimate.com'
 end
