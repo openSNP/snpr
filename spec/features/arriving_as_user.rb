78a38bd95334ecb734ffb7aa30fe4ced3758146a
Philipp Bayer <PhilippBay@gmail.com>
2016-08-12 22:50:22 +0800

    Puts more in test

--- spec/features/arriving_as_user.rb
+++ spec/features/arriving_as_user.rb
@@ -1,12 +1,14 @@
 RSpec.describe 'Arriving as user' do
-  let!(:user) { create(:user, name: 'Potato Bill') }
+  let(:snp) { double('snp', name: 'rs1234', snpedia_papers: [snpedia_paper]) }
+  let!(:user) { create(:user, name: 'Potato Bill', snps: [snp]) }
+
   let(:snpedia_paper) do
     double(:snpedia_paper, url: 'http://www.snpedia.com/index.php/Rs1234(A;C)',
                            summary: 'Green hair',
                            snp_variation: 'AC',
-                           created_at: Time.new(2016).to_s)
+                           created_at: Time.new(2016).to_s,
+                           id: 1)
   end
-  let(:snp) { double('snp', name: 'rs1234', snpedia_papers: [snpedia_paper]) }
 
   context 'as a signed-in user' do
 
@@ -18,7 +20,10 @@ RSpec.describe 'Arriving as user' do
       visit root_path
 
       expect(page).to have_content('Hello Potato!')
+      expect(page).to have_content('rs1234')
 
+      # Test Your last 30 updated SNPs feed
+      expect(page).to have_content('Received new data from Mendeley')
     end
   end
 
