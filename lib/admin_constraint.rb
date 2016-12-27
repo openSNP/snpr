0ce0e3b1357524de60cfa6abc0b842618156bf79
Helge Rausch <helge@rausch.io>
2016-12-27 13:29:21 +0100

    Address another Rubocop complaint

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -2,7 +2,8 @@
 class AdminConstraint
   def matches?(request)
     return false unless request.cookie_jar['user_credentials'].present?
-    user = User.find_by_persistence_token(request.cookie_jar['user_credentials'].split(':')[0])
+    token = request.cookie_jar['user_credentials'].split(':').first
+    user = User.find_by(persistence_token: token)
     user && user.admin?
   end
 end
