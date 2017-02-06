e9537b4f957445194339adc0c6829dbb90253a39
Bastian Greshake <bgreshake@googlemail.com>
2017-02-06 14:40:39 +0100

    roll back to helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,26 @@ module ApplicationHelper
     link_to title, { sort: column, direction: direction }, class: css_class
   end
 
+  class BootstrapLinkRenderer < ::WillPaginate::ActionView::LinkRenderer
+    protected
+
+    def html_container(html)
+      tag :div, tag(:ul, html), container_attributes
+    end
+
+    def page_number(page)
+      tag :li, link(page, page, rel: rel_value(page)), class: ('active' if page == current_page)
+    end
+
+    def gap
+      tag :li, link('&hellip;'.html_safe, '#'), class: 'disabled'
+    end
+
+    def previous_or_next_page(page, text, classname)
+      tag :li, link(text, page || '#'), class: [classname[0..3], classname, ('disabled' unless page)].join(' ')
+    end
+  end
+
   def page_navigation_links(pages)
     will_paginate(pages, class: 'pagination', inner_window: 2, outer_window: 0, renderer: BootstrapLinkRenderer, previous_label: '&larr;'.html_safe, next_label: '&rarr;'.html_safe, page_links: false)
   end
