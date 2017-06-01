f57b7d7696e052d8653540b50b73768dfad8398c
Philipp Bayer <PhilippBay@gmail.com>
2017-06-01 14:07:06 +0800

    Changed some stuff in fitbit notification worker - prob. needs bigger refactor, long lines, cannot change

--- app/workers/fitbit_notification.rb
+++ app/workers/fitbit_notification.rb
@@ -15,11 +15,11 @@ class FitbitNotification
          if n['collectionType'] == 'activities' and @fitbit_profile.activities == true
            @activity = FitbitActivity.find_or_create_by_fitbit_profile_id_and_date_logged(@fitbit_profile.id, n['date'])
            @steps = @client.data_by_time_range('/activities/log/steps',
-                                               { base_date: n['date'],
-                                                period: '1d' })['activities-log-steps'][0]['value']
+                                               base_date: n['date'],
+                                               period: '1d' )['activities-log-steps'][0]['value']
            @floors = @client.data_by_time_range('/activities/log/floors',
-                                                { base_date: n['date'],
-                                                 period: '1d' })['activities-log-floors'][0]['value']
+                                                base_date: n['date'],
+                                                period: '1d' )['activities-log-floors'][0]['value']
            @activity.steps = @steps
            @activity.floors = @floors
            @activity.save
@@ -27,27 +27,27 @@ class FitbitNotification
            @sleep = FitbitSleep.find_or_create_by_fitbit_profile_id_and_date_logged(@fitbit_profile.id,
                                                                                     n['date'])
            @sleep.minutes_asleep = @client.data_by_time_range('/sleep/minutesAsleep',
-                                                              { base_date: n['date'],
-                                                               period: '1d' })['sleep-minutesAsleep'][0]['value']
+                                                              base_date: n['date'],
+                                                              period: '1d' )['sleep-minutesAsleep'][0]['value']
            @sleep.number_awakenings = @client.data_by_time_range('/sleep/awakeningsCount',
-                                                                 { base_date: n['date'],
-                                                                  period: '1d' })['sleep-awakeningsCount'][0]['value']
+                                                                 base_date: n['date'],
+                                                                 period: '1d' )['sleep-awakeningsCount'][0]['value']
            @sleep.minutes_awake = @client.data_by_time_range('/sleep/minutesAwake',
-                                                             { base_date: n['date'],
-                                                              period: '1d' })['sleep-minutesAwake'][0]['value']
+                                                             base_date: n['date'],
+                                                             period: '1d' )['sleep-minutesAwake'][0]['value']
            @sleep.minutes_to_sleep = @client.data_by_time_range('/sleep/minutesToFallAsleep',
-                                                                { base_date: n['date'],
-                                                                 period: '1d' })['sleep-minutesToFallAsleep'][0]['value']
+                                                                base_date: n['date'],
+                                                                period: '1d' )['sleep-minutesToFallAsleep'][0]['value']
            @sleep.save
          elsif n['collectionType'] == 'body'
            @body = FitbitBody.find_or_create_by_fitbit_profile_id_and_date_logged(@fitbit_profile.id,
                                                                                   n['date'])
            @body.bmi = @client.data_by_time_range('/body/bmi',
-                                                  { base_date: n['date'],
-                                                   period: '1d' })['body-bmi'][0]['value']
+                                                  base_date: n['date'],
+                                                  period: '1d' )['body-bmi'][0]['value']
            @body.weight = @client.data_by_time_range('/body/weight',
-                                                     { base_date: n['date'],
-                                                      period: '1d' })['body-weight'][0]['value']
+                                                     base_date: n['date'],
+                                                     period: '1d' )['body-weight'][0]['value']
            @body.save
          end
        end
