98c1943c1ae75e542b0931097254a279a2421a86
Bastian Greshake <bgreshake@googlemail.com>
2017-06-06 16:16:04 +0200

    [MRG] Adding more tests (#390)
    * added more tests for snp page
    
    * 🐶  happiness
    
    * tests for changing password
    
    * 🐶  happiness
    
    * helge happiness ;)

---
+++ spec/features/password_change_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Password changing' do
+  let!(:user) { create(:user, name: 'Queen Anne', email: 'anne@the.uk') }
+
+  context 'as a signed-in user' do
+    before do
+      sign_in(user)
+    end
+
+    scenario 'the password is too short' do
+      visit "/users/#{user.id}/changepassword"
+      fill_in('Password', with: '1710')
+      fill_in('Confirmation', with: '1710')
+      click_on('Update Information')
+      expect(page).not_to have_content("Password confirmation doesn't match Password")
+      expect(page).to have_content('Password is too short')
+    end
+
+    scenario 'confirmation doesnt match' do
+      visit "/users/#{user.id}/changepassword"
+      fill_in('Password', with: 'yetanothermary')
+      fill_in('Confirmation', with: 'yetanothermarry')
+      click_on('Update Information')
+      expect(page).to have_content("Password confirmation doesn't match Password")
+      expect(page).not_to have_content('Password is too short')
+    end
+
+    scenario 'finally got it right' do
+      visit "/users/#{user.id}/changepassword"
+      fill_in('Password', with: 'QueenAnnesRevenge')
+      fill_in('Confirmation', with: 'QueenAnnesRevenge')
+      click_on('Update Information')
+      expect(page).not_to have_content("Password confirmation doesn't match Password")
+      expect(page).not_to have_content('Password is too short')
+      click_on('Queen Anne')
+      click_on('Sign out')
+      visit '/signin'
+      fill_in('Email', with: 'anne@the.uk')
+      fill_in('Password', with: 'QueenAnnesRevenge')
+      click_on('Login')
+      expect(page).to have_content('Login successful!')
+    end
+  end
+end
