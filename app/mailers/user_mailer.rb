4e5d56d124e18dc589130f46ebbfb2b4c180dfc9
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-11-10 13:53:35 -0800

    Newsletter (#437)
    * newsletter 2017-11 first draft
    
    * fixed views
    
    * edited subject
    
    * edited roadmap

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -97,8 +97,8 @@ class UserMailer < ActionMailer::Base
                          password: ENV.fetch('SURVEY_EMAIL_PASSWORD'),
                          address: ENV.fetch('SURVEY_EMAIL_ADDRESS'),
                          port: ENV.fetch('SURVEY_EMAIL_PORT') }
-    mail(subject: 'openSNP: $5k USD grants, job opportunity, '\
-                  '& Zurich smell/taste genetics workshop Sept 2-3',
+    mail(subject: 'openSNP: Winner of the CrowdAI machine learning competition'\
+                  ' & a survey on genetic raw data usage',
          to: @user.email,
          from: 'survey@opensnp.org',
          delivery_method_options: delivery_options)
