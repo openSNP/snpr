351c023d5b0ddf387f98b0474e07200888c62057
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-11-15 10:59:59 -0800

    further refactor

--- app/controllers/open_humans_profiles_controller.rb
+++ app/controllers/open_humans_profiles_controller.rb
@@ -6,7 +6,7 @@ class OpenHumansProfilesController < ApplicationController
   def start_auth
     # there's little to do for the start. Just read the .env for our client_id
     # then lead ppl to OH.org to give us a key
-    client_id = ENV.fetch('OH_client_id')
+    client_id = ENV.fetch('OH_CLIENT_ID')
     base_url = 'https://www.openhumans.org/direct-sharing/projects/oauth2/authorize/?client_id='
     redirect_to base_url + "#{client_id}&response_type=code"
   end
@@ -15,20 +15,22 @@ class OpenHumansProfilesController < ApplicationController
     # let's get the current user and their code
     @user = current_user
     @code = params[:code]
-    oh_service = OpenHumansService.new
 
     # does the user have an OH profile on openSNP? if not, create one
     if @user.open_humans_profile.nil?
       @user.open_humans_profile = OpenHumansProfile.new
       @user.save
     end
+
+    oh_service = OpenHumansService.new(@user)
+
     # lets convert
-    oh_service.get_access_tokens(@user, @code)
-    oh_service.set_open_humans_ids(@user.open_humans_profile)
+    oh_service.get_access_tokens(@code)
+    oh_service.set_open_humans_ids
     # delete old files if there are any
     begin
-      oh_service.delete_opensnp_id(@user)
+      oh_service.delete_opensnp_id
     end
-    oh_service.upload_opensnp_id(@user)
+    oh_service.upload_opensnp_id
   end
 end
--- app/services/open_humans_service.rb
+++ app/services/open_humans_service.rb
@@ -2,110 +2,109 @@
 
 class OpenHumansService
   # used to do the interfacing w/ Open Humans
+  BASE_API_URL = 'https://www.openhumans.org/api/direct-sharing/project'
+  BOUNDARY = '0P3NSNPH34RT50PENHUM4N5'
 
-  def get_access_tokens(user, code)
+  def initialize(user)
+    @user = user
+    @oh_profile = @user.open_humans_profile
+  end
+
+  def get_access_tokens(code)
     # authenticate w/ our client id/secret against API
     # post with the key the user provided us with.
     url = URI.parse('https://www.openhumans.org/oauth2/token/')
     req = Net::HTTP::Post.new(url.request_uri)
-    req.basic_auth ENV.fetch('OH_client_id'), ENV.fetch('OH_client_secret')
+    req.basic_auth ENV.fetch('OH_CLIENT_ID'), ENV.fetch('OH_CLIENT_SECRET')
     req.set_form_data('grant_type' => 'authorization_code',
                       'code' => code,
-                      'redirect_uri' => 'http://localhost:3000/openhumans/authorize')
-    # set up request to use https
+                      'redirect_uri' => OH_REDIRECT_URL)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = (url.scheme == 'https')
-    # do actual request, get the json it returns
     response = http.request(req)
-    response_json = JSON.parse(response.body)
-    update_tokens(user.open_humans_profile, response_json)
+    update_tokens(JSON.parse(response.body))
   end
 
-  def refresh_token(user)
-    oh_profile = user.open_humans_profile
+  def refresh_token
     url = URI.parse('https://www.openhumans.org/oauth2/token/')
     req = Net::HTTP::Post.new(url.request_uri)
-    req.set_form_data('grant_type' => 'refresh_token', 'refresh_token' => oh_profile.refresh_token)
+    req.set_form_data('grant_type' => 'refresh_token',
+                      'refresh_token' => @oh_profile.refresh_token)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = (url.scheme == 'https')
     response = http.request(req)
-    response_json = JSON.parse(response.body)
-    update_tokens(oh_profile, response_json)
+    update_tokens(JSON.parse(response.body))
   end
 
-  def set_open_humans_ids(oh_profile)
-    uri = URI.parse('https://www.openhumans.org/api/direct-sharing/project/exchange-member/')
-    uri_params = { access_token: oh_profile.access_token }
+  def set_open_humans_ids
+    uri = URI.parse(BASE_API_URL + '/exchange-member/')
+    uri_params = { access_token: @oh_profile.access_token }
     uri.query = URI.encode_www_form(uri_params)
     res = Net::HTTP.get_response(uri)
     res_json = JSON.parse(res.body)
-    oh_profile.project_member_id = res_json['project_member_id']
-    oh_profile.open_humans_user_id = res_json['username']
-    oh_profile.save
+    @oh_profile.project_member_id = res_json['project_member_id']
+    @oh_profile.open_humans_user_id = res_json['username']
+    @oh_profile.save
   end
 
-  def upload_opensnp_id(user)
-    base_url = 'https://www.openhumans.org/api/direct-sharing/project/files/upload/?access_token='
-    uri = URI.parse(base_url + user.open_humans_profile.access_token)
-    boundary = '0P3NSNPH34RT50PENHUM4N5'
+  def upload_opensnp_id
+    base_url = BASE_API_URL + '/files/upload/?access_token='
+    uri = URI.parse(base_url + @oh_profile.access_token)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == 'https')
     upload = Net::HTTP::Post.new(uri.request_uri)
-    upload.content_type = "multipart/form-data; boundary=#{boundary}"
-    post_body = generate_form_body(user, boundary)
-    upload.body = post_body.join
+    upload.content_type = "multipart/form-data; BOUNDARY=#{BOUNDARY}"
+    upload.body = generate_form_body
     http.request(upload)
   end
 
-  def delete_opensnp_id(user)
-    base_url = 'https://www.openhumans.org/api/direct-sharing/project/files/delete/?access_token='
-    url = URI.parse(base_url + user.open_humans_profile.access_token)
+  def delete_opensnp_id
+    base_url = BASE_API_URL + '/files/delete/?access_token='
+    url = URI.parse(base_url + @oh_profile.access_token)
     req = Net::HTTP::Post.new(url.request_uri)
-    oh_profile = user.open_humans_profile
-    req.set_form_data(project_member_id: oh_profile.project_member_id, all_files: true)
+    req.set_form_data(project_member_id: @oh_profile.project_member_id, all_files: true)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = (url.scheme == 'https')
-    # http.set_debug_output($stdout)
     http.request(req)
   end
 
   private
 
-  def update_tokens(oh_profile, tokens)
-    oh_profile.expires_in = Time.current + tokens['expires_in']
-    oh_profile.access_token = tokens['access_token']
-    oh_profile.refresh_token = tokens['refresh_token']
-    oh_profile.save
+  def update_tokens(tokens)
+    @oh_profile.expires_in = Time.current + tokens['expires_in']
+    @oh_profile.access_token = tokens['access_token']
+    @oh_profile.refresh_token = tokens['refresh_token']
+    @oh_profile.save
   end
 
-  def generate_json(user)
-    { user_name: user.name,
-      user_id: user.id,
-      has_sequence: user.has_sequence,
-      user_uri: "https://opensnp.org/users/#{user.id}" }
+  def generate_json
+    { user_name: @user.name,
+      user_id: @user.id,
+      has_sequence: @user.has_sequence,
+      user_uri: "https://opensnp.org/users/#{@user.id}" }
   end
 
-  def generate_metadata(user)
-    metadata = generate_json(user)
+  def generate_metadata
+    metadata = generate_json()
     metadata['tags'] = ['opensnp']
-    metadata['description'] = "links to openSNP user #{user.id}"
+    metadata['description'] = "links to openSNP user #{@user.id}"
     return metadata
   end
 
-  def generate_form_body(user, boundary)
-    metadata = generate_metadata(user)
-    file_content = generate_json(user)
-    post_body = []
-    post_body << "--#{boundary}\r\n"
-    post_body << "Content-Disposition: form-data; name=\"project_member_id\"\r\n\r\n"
-    post_body << user.open_humans_profile.project_member_id
-    post_body << "\r\n--#{boundary}\r\n"
-    post_body << "Content-Disposition: form-data; name=\"metadata\"\r\n\r\n"
-    post_body << metadata.to_json
-    post_body << "\r\n--#{boundary}\r\n"
-    post_body << "Content-Disposition: form-data; "
-    post_body << "name=\"data_file\"; filename=\"#{user.id}.json\"\r\n\r\n"
-    post_body << file_content.to_json
-    post_body << "\r\n\r\n--#{boundary}--\r\n"
+  def generate_form_body
+    metadata = generate_metadata
+    file_content = generate_json
+    <<-POST_BODY.strip_heredoc
+      --#{BOUNDARY}\r
+      Content-Disposition: form-data; name="project_member_id"\r\n\r
+      #{@oh_profile.project_member_id}\r
+      --#{BOUNDARY}\r
+      Content-Disposition: form-data; name="metadata"\r\n\r
+      #{metadata.to_json}\r
+      --#{BOUNDARY}\r
+      Content-Disposition: form-data; name="data_file"; filename="#{@user.id}.json"\r\n\r
+      #{file_content.to_json}\r
+      --#{BOUNDARY}--\r\n\r
+    POST_BODY
   end
 end
