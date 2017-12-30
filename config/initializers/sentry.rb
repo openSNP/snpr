264d3578f05414c15764fd01a01afa0d1f6578ba
Helge Rausch <helge@rausch.io>
2017-12-30 17:25:50 +0100

    Setup sentry.io for exception tracking

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = ENV.fetch('SENTRY_DSN')
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
+end
