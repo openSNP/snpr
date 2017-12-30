7e6c55854a0d9f0252ed5ba56c31059a1cef8717
Helge Rausch <helge@rausch.io>
2017-12-30 20:48:29 +0100

    Hound happiness 🐶

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -5,14 +5,14 @@ class UserSession < Authlogic::Session::Base
 
   # rails 3 broke something, this is for fix
   def to_key
-    new_record? ? nil : [self.send(self.class.primary_key)]
+    new_record? ? nil : [send(self.class.primary_key)]
   end
 
   def raven_set_user_context
     Raven.user_context(
-      'id' => self.user.id,
-      'email' => self.user.email,
-      'username' => self.user.name
+      'id' => user.id,
+      'email' => user.email,
+      'username' => user.name
     )
   end
 
