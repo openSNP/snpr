2fb12d7ad9ab0607a6731048fa1cf20727b7fca0
Helge Rausch <helge@rausch.io>
2017-12-30 20:47:06 +0100

    Set user context

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 class UserSession < Authlogic::Session::Base
+  after_persisting :raven_set_user_context
+  after_destroy :raven_clear_user_context
 
-	# rails 3 broke something, this is for fix
-	def to_key
-		new_record? ? nil : [ self.send(self.class.primary_key) ]
-	end
+  # rails 3 broke something, this is for fix
+  def to_key
+    new_record? ? nil : [self.send(self.class.primary_key)]
+  end
+
+  def raven_set_user_context
+    Raven.user_context(
+      'id' => self.user.id,
+      'email' => self.user.email,
+      'username' => self.user.name
+    )
+  end
+
+  def raven_clear_user_context
+    Raven.user_context({})
+  end
 end
