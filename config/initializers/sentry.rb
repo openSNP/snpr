d03b3ae1897f3c6eff18b4f2a1430bd0183e3faa
Helge Rausch <helge@rausch.io>
2017-12-31 16:51:48 +0100

    Track releases in Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,5 +4,6 @@ if Rails.env.production?
   Raven.configure do |config|
     config.dsn = ENV.fetch('SENTRY_DSN')
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.release = File.read('RELEASE').strip
   end
 end
