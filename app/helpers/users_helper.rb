969aa5f56186ee2d9ca8c8db48a5597788214bc3
Helge Rausch <helge@rausch.io>
2018-01-02 13:10:39 +0100

    Order parse status cases logically

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def display_parse_status(genotype)
     label_class = case genotype.parse_status
-                  when 'done' then 'label-success'
                   when 'queued' then 'label-default'
                   when 'parsing' then 'label-primary'
+                  when 'done' then 'label-success'
                   when 'error' then 'label-danger'
                   end
     content_tag('span', genotype.parse_status, class: "label #{label_class}")
