99d32204cf279e42eadcbe610419ce362bd7933b
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-01-18 10:11:30 -0800

    try fixing migration

--- db/migrate/20180118100003_remove_fitbit.rb
+++ db/migrate/20180118100003_remove_fitbit.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 class RemoveFitbit < ActiveRecord::Migration
-  def change
+  def self.up
     drop_table :fitbit_activities
     drop_table :fitbit_sleeps
     drop_table :fitbit_bodies
     drop_table :fitbit_profiles
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
