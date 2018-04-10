bf4ee4cf5bc16d82c9d996e36b80c7007a3d57c9
Helge Rausch <helge@rausch.io>
2018-04-10 15:03:52 +0200

    Explicitly set file permissions to 0644

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Paperclip::Attachment.default_options[:overwrite_file_permissions] = false
+Paperclip::Attachment.default_options[:overwrite_file_permissions] = 0o644
