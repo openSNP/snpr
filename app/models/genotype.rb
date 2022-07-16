f43dbc4794e2de429a9431cffd1624f0e865aae5
Helge Rausch <helge@rausch.io>
2022-07-16 10:53:19 +0200

    Fix tests (hopefully)

--- app/models/genotype.rb
+++ app/models/genotype.rb
@@ -7,12 +7,12 @@ class Genotype < ActiveRecord::Base
   validates_presence_of :user
 
   has_attached_file :genotype, url: '/data/:fs_filename',
-                               path: "#{Rails.root}/public/data/:fs_filename"
+                               path: "#{Rails.root}/public/data/:fs_filename",
+                               validate_media_type: false
   before_post_process :is_image?
   validates_attachment :genotype,
     presence: true,
     size: { in: 0..400.megabytes }
-  do_not_validate_attachment_file_type :genotype
 
   def is_image?
     false
