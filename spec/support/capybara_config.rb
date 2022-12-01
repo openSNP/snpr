e3c1ade8533fad7dc6a97e9e7dab0a4bf3ea38ba
philippbayer <philippbay@gmail.com>
2022-12-01 21:20:54 +0800

    make hound happy

--- spec/support/capybara_config.rb
+++ spec/support/capybara_config.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
 # from https://www.railsagency.com/blog/2020/03/11/how-to-configure-full-stack-integration-testing-with-selenium-and-ruby-on-rails/
 # lots of options, but gets around 'exit code 1' on github
 Capybara.register_driver :chrome_headless do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     'goog:chromeOptions': {
-      args: %w[ no-sandbox headless disable-gpu --window-size=1920,1080]
+      args: %w[no-sandbox headless disable-gpu --window-size=1920,1080]
     }
   )
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
 
-#Ensure Log directory exists
-%x(mkdir -p tmp/selenium_logs)
+# Ensure Log directory exists
+`mkdir -p tmp/selenium_logs`
 
 Capybara.register_driver :chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions': { args: %w[ start-maximized ] }
+    'goog:chromeOptions': { args: %w[start-maximized] }
   )
 
   Capybara::Selenium::Driver.new(
