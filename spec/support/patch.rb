74da535e0330679d511ad4f581717252af6c8c6b
philippbayer <philippbay@gmail.com>
2022-12-02 10:51:13 +0800

    different monkeypatch

--- spec/support/patch.rb
+++ spec/support/patch.rb
@@ -1,14 +1,17 @@
 # Rails 4.2 call `initialize` inside `recycle!`. However Ruby 2.6 doesn't allow calling `initialize` twice.
 # Rails refuse to patch that as Rails 4 is old.
 # See for detail: https://github.com/rails/rails/issues/34790
-if RUBY_VERSION.to_f >= 2.6 && Rails::VERSION::MAJOR == 4
-  class ActionController::TestResponse
-    prepend Module.new {
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
       def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
         @mon_mutex_owner_object_id = nil
         @mon_mutex = nil
-        super
+        initialize
       end
-    }
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
   end
 end
