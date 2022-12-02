478725dea5b043e94323f54857820bd9d9bdfddb
philippbayer <philippbay@gmail.com>
2022-12-02 12:45:00 +0800

    Give the monkeypatch to regular tests, too

--- spec/support/patch.rb
+++ spec/support/patch.rb
@@ -29,15 +29,6 @@ if rb_version >= Gem::Version.new('2.6') && Gem::Version.new(Rails.version) < Ge
       end
     end
 
-    class ActionController::TestCase < ActiveSupport::TestCase
-      def recycle!
-        @body = nil
-        @mon_data = nil
-        @mon_data_owner_object_id = nil
-        initialize
-      end
-    end
-
   else
     puts "Using #{__FILE__} for Ruby 2.6."
 
