bdf47ae18a9576f80210bac2cd16775befbdadc2
Helge Rausch <helge@rausch.io>
2023-08-12 10:23:48 +0200

    fixup! Upgrade Rails to 5.1

--- app/models/user_snp.rb
+++ app/models/user_snp.rb
@@ -3,8 +3,8 @@
 class UserSnp < ApplicationRecord
   self.primary_keys = [:genotype_id, :snp_name]
   belongs_to :snp, foreign_key: :snp_name, primary_key: :name, counter_cache: true
-  has_one :user, through: :genotype
   belongs_to :genotype
+  has_one :user, through: :genotype
 
   validates_presence_of :snp
   validates_presence_of :genotype
