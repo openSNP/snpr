570918cb471f8e5ec635caf0dd6082423962d518
Helge Rausch <helge@rausch.io>
2024-07-10 09:22:19 +0200

    Delete capistrano files

--- lib/capistrano/tasks/assets.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-namespace :deploy do
-  task :precompile_assets do
-    run "cd '#{release_path}'; RAILS_GROUPS=assets RAILS_ENV=production rake assets:precompile"
-  end
-  after "deploy:create_shared_dirs", "deploy:precompile_assets"
-end
--- lib/capistrano/tasks/passenger.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-namespace :passenger do
-  task :restart do
-    run("cd #{current_path}; touch tmp/restart.txt")
-  end
-end
